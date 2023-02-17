a45e0f6062187711ebcaf5d16b9700eca1f4858b
mjdamgaard <mjdamgaard@outlook.dk>
2023-02-17 18:09:20 +0100

    commit before changing TEXTs to VARCHARs.

--- sql/create_public_sdb.sql
+++ sql/create_public_sdb.sql
@@ -1,23 +1,62 @@
-CREATE TABLE mydatabase.SemanticInputs (
+USE mydatabase;
+
+-- DROP TABLE Users;
+-- DROP TABLE SemanticInputs;
+-- DROP TABLE Statements;
+-- DROP TABLE DescribedTerms;
+-- DROP TABLE TTexts;
+-- DROP TABLE STexts;
+-- DROP TABLE MTexts;
+-- DROP TABLE TBlobs;
+-- DROP TABLE SBlobs;
+-- DROP TABLE MBlobs;
+-- DROP TABLE L1Lists;
+-- DROP TABLE L2Lists;
+-- DROP TABLE L3Lists;
+-- DROP TABLE L4Lists;
+
+
+
+-- type code for User: 0.
+CREATE TABLE Users (
+	/* user ID */
+	id BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(id),
+
+	/* primary fields */
+	public_encryption_key BIGINT,
+
+	/* database types (tables) of primary fields */
+		/* public_encryption_key types */
+		-- allowed public_encryption_key types: TBLOB (so no flag needed).
+	/**/
+
+	/* timestamp */
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- type code for SemanticInput: 1.
+CREATE TABLE SemanticInputs (
 	/* semantic input ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
 	/* primary fields */
-	user BIGINT FOREIGN KEY,
-	statement BIGINT FOREIGN KEY,
+	user BIGINT,
+	statement BIGINT,
 	value BIGINT,
-	
+
 	/* database types (tables) of primary fields */
 		/* user types */
 		-- allowed user types: only User (so no flag needed).
 
 		/* statement types */
-		-- allowed statement types: MonadicStatement or DyadicStatement.
-		statement_type TINYINT CHECK (
-			statement_type = 1 OR -- (MonadicStatement)
-			statement_type = 2    -- (DyadicStatement)
-		),
+		-- allowed statement types: only Statement (so no flag needed).
+			-- OBSOLETE:
+			-- statement_type TINYINT CHECK (
+			-- 	statement_type = 1 OR -- (MonadicStatement)
+			-- 	statement_type = 2    -- (DyadicStatement)
+			-- ),
 
 		/* value types */
 			-- (OBSOLETE: allowed value types: only BIGINT (so no flag needed).)
@@ -26,19 +65,19 @@ CREATE TABLE mydatabase.SemanticInputs (
 	/**/
 
 	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for SemanticInput: 0.
 
 
-CREATE TABLE mydatabase.MonadicStatements (
-	/* monadic statement ID */
+-- type code for Statement: 2.
+CREATE TABLE Statements (
+	/* statement ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
 	/* primary fields */
 	subject BIGINT,
-	predicate BIGINT FOREIGN KEY,
+	predicate BIGINT,
 
 	/* database types (tables) of primary fields */
 		/* subject types */
@@ -46,242 +85,227 @@ CREATE TABLE mydatabase.MonadicStatements (
 		subject_type TINYINT,
 
 		/* predicate types */
-		-- allowed predicate types: only DescribedTerm (so no flag needed).
-	/**/
-	
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
--- type code for MonadicStatement: 1.
-
-
-CREATE TABLE mydatabase.DyadicStatements (
-	/* dyadic statement ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
-
-	/* primary fields */
-	subject BIGINT,
-	relation BIGINT FOREIGN KEY,
-	object BIGINT,
-
-	/* database types (tables) of primary fields */
-		/* subject types */
-		-- allowed subject types: any (so no constraints).
-		subject_type TINYINT,
-
-		/* predicate types */
-		-- allowed predicate types: only DescribedTerm (so no flag needed).
-
-		/* object types */
-		-- allowed object types: any (so no constraints).
-		object_type TINYINT,
+		-- allowed predicate types: DescribedTerm or FunctionalTerm.
+		statement_type TINYINT CHECK (
+			statement_type = 3 OR -- (DescribedTerm)
+			statement_type = 4    -- (FunctionalTerm)
+		)
 	/**/
 
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	-- /* user (or bot) responsible for upload */
+	-- user BIGINT,
+	--
+	-- /* timestamp */
+	-- createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for DyadicStatement: 2.
 
 
-CREATE TABLE mydatabase.DescribedTerms (
+
+
+-- CREATE TABLE DyadicStatements (
+-- 	/* dyadic statement ID */
+-- 	id BIGINT AUTO_INCREMENT,
+-- 	PRIMARY KEY(id),
+
+-- 	/* primary fields */
+-- 	subject BIGINT,
+-- 	relation BIGINT,
+-- 	object BIGINT,
+
+-- 	/* database types (tables) of primary fields */
+-- 		/* subject types */
+-- 		-- allowed subject types: any (so no constraints).
+-- 		subject_type TINYINT,
+
+-- 		/* predicate types */
+-- 		-- allowed predicate types: only DescribedTerm (so no flag needed).
+
+-- 		/* object types */
+-- 		-- allowed object types: any (so no constraints).
+-- 		object_type TINYINT,
+-- 	/**/
+
+-- 	/* user (or bot) responsible for upload */
+-- 	user BIGINT,
+
+-- 	/* timestamp */
+-- 	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+
+
+
+-- type code for DescribedTerm: 3.
+CREATE TABLE DescribedTerms (
 	/* described term ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
 	/* primary fields */
-	description BIGINT FOREIGN KEY,
+	description_schema BIGINT,
+	schema_input BIGINT,
 
 	/* database types (tables) of primary fields */
 		/* subject types */
 		-- allowed subject types: TText or SText.
-		description_type TINYINT CHECK (
-			description_type = 11 OR -- (TText)
-			description_type = 12    -- (SText)
+		description_schema_type TINYINT CHECK (
+			description_schema_type = 11 OR -- (TText)
+			description_schema_type = 12    -- (SText)
 		),
+
+		/* schema_input types */
+		-- allowed schema_input types: any (so no constraints).
+		schema_input_type TINYINT
 	/**/
 
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	-- /* user (or bot) responsible for upload */
+	-- user BIGINT,
+	--
+	-- /* timestamp */
+	-- createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for DescribedTerm: 3.
+
+
+-- -- type code for FunctionalTerm: 4.
+-- CREATE TABLE FunctionalTerms (
+-- 	/* functional term ID */
+-- 	id BIGINT AUTO_INCREMENT,
+-- 	PRIMARY KEY(id),
+--
+-- 	/* primary fields */
+-- 	function BIGINT,
+-- 	input BIGINT,
+--
+-- 	/* database types (tables) of primary fields */
+-- 		/* input types */
+-- 		-- allowed input types: any (so no constraints).
+-- 		subject_type TINYINT,
+--
+-- 		/* function types */
+-- 		-- allowed function types: DescribedTerm or FunctionalTerm.
+-- 		statement_type TINYINT CHECK (
+-- 			statement_type = 3 OR -- (DescribedTerm)
+-- 			statement_type = 4    -- (FunctionalTerm)
+-- 		)
+-- 	/**/
+--
+-- 	-- /* user (or bot) responsible for upload */
+-- 	-- user BIGINT,
+-- 	--
+-- 	-- /* timestamp */
+-- 	-- createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
 
 
 
-CREATE TABLE mydatabase.Users (
-	/* user ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+-- type code for DateTime: 6.
+-- type code for Year: 7.
+-- type code for Date: 8.
+-- type code for Time: 9.
 
-	/* primary fields */
-	public_encryption_key BIGINT FOREIGN KEY,
-
-	/* database types (tables) of primary fields */
-		/* public_encryption_key types */
-		-- allowed public_encryption_key types: TBLOB (so no flag needed).
-	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
--- type code for User: 5.
-
-
--- type code for DateTime: 6. 
--- type code for Year: 7. 
--- type code for Date: 8. 
--- type code for Time: 9. 
-
--- type code for Bool 10. 
--- type code for TinyInt: 11. 
--- type code for SmallInt: 12. 
--- type code for MediumInt: 13. 
--- type code for Int: 14. 
--- type code for BigInt: 18. 
+-- type code for Bool 10.
+-- type code for TinyInt: 11.
+-- type code for SmallInt: 12.
+-- type code for MediumInt: 13.
+-- type code for Int: 14.
+-- type code for BigInt: 18.
 
 
 
-CREATE TABLE mydatabase.TTexts (
+-- type code for TText: 21.
+CREATE TABLE TTexts (
 	/* tiny text ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
 	/* primary fields */
-	t_text TINYTEXT,
+	t_text TINYTEXT
 
 	/* database types (tables) of primary fields */
 		/* t_text types */
 		-- allowed t_text types: TINYTEXT (so no flag needed).
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for TText: 21.
 
-CREATE TABLE mydatabase.STexts (
+-- type code for SText: 22.
+CREATE TABLE STexts (
 	/* small text ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
 	/* primary fields */
-	s_text TEXT,
+	s_text TEXT
 
 	/* database types (tables) of primary fields */
 		/* s_text types */
 		-- allowed s_text types: TEXT (so no flag needed).
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for SText: 22.
 
-CREATE TABLE mydatabase.MTexts (
+-- type code for MText: 23.
+CREATE TABLE MTexts (
 	/* medium text ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
 	/* primary fields */
-	m_text MEDIUMTEXT,
+	m_text MEDIUMTEXT
 
 	/* database types (tables) of primary fields */
 		/* m_text types */
 		-- allowed m_text types: MEDIUMTEXT (so no flag needed).
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for MText: 23.
 
 -- saving LTexts for later.
+-- saving DeltaTexts and CompoundTexts for later.
 
 
 
-CREATE TABLE mydatabase.TBlobs (
+-- type code for TBlob: 31.
+CREATE TABLE TBlobs (
 	/* tiny BLOB ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
 	/* primary fields */
-	t_blob TINYBLOB,
+	t_blob TINYBLOB
 
 	/* database types (tables) of primary fields */
 		/* t_blob types */
 		-- allowed t_blob types: TINYBLOB (so no flag needed).
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for TBlob: 31.
 
-CREATE TABLE mydatabase.SBlobs (
+-- type code for SBlob: 32.
+CREATE TABLE SBlobs (
 	/* small BLOB ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
 	/* primary fields */
-	s_blob BLOB,
+	s_blob BLOB
 
 	/* database types (tables) of primary fields */
 		/* s_blob types */
 		-- allowed s_blob types: BLOB (so no flag needed).
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for SBlob: 32.
 
-CREATE TABLE mydatabase.MBlobs (
+-- type code for MBlob: 33.
+CREATE TABLE MBlobs (
 	/* medium BLOB ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
 	/* primary fields */
-	m_blob MEDIUMBLOB,
+	m_blob MEDIUMBLOB
 
 	/* database types (tables) of primary fields */
 		/* m_blob types */
 		-- allowed m_blob types: MEDIUMBLOB (so no flag needed).
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for MBlob: 33.
 
 -- saving LBlobs for later.
 
@@ -290,7 +314,8 @@ CREATE TABLE mydatabase.MBlobs (
 -- (List0 does not need its own table.)
 -- type code for L0List: 40.
 
-CREATE TABLE mydatabase.L1Lists (
+-- type code for L1List: 41.
+CREATE TABLE L1Lists (
 	/* length 1 list ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
@@ -301,18 +326,12 @@ CREATE TABLE mydatabase.L1Lists (
 	/* database types (tables) of primary fields */
 		/* element types */
 		-- allowed element types: any (so no constraints).
-		element_1_type TINYINT,
+		element_1_type TINYINT
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for L1List1: 41.
 
-CREATE TABLE mydatabase.L2Lists (
+-- type code for L2List: 42.
+CREATE TABLE L2Lists (
 	/* length 2 list ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
@@ -325,18 +344,12 @@ CREATE TABLE mydatabase.L2Lists (
 		/* element types */
 		-- allowed element types: any (so no constraints).
 		element_1_type TINYINT,
-		element_2_type TINYINT,
+		element_2_type TINYINT
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for L2List1: 42.
 
-CREATE TABLE mydatabase.L3Lists (
+-- type code for L3List: 43.
+CREATE TABLE L3Lists (
 	/* length 2 list ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
@@ -351,18 +364,12 @@ CREATE TABLE mydatabase.L3Lists (
 		-- allowed element types: any (so no constraints).
 		element_1_type TINYINT,
 		element_2_type TINYINT,
-		element_3_type TINYINT,
+		element_3_type TINYINT
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for L3List1: 43.
 
-CREATE TABLE mydatabase.L4Lists (
+-- type code for L3List: 44.
+CREATE TABLE L4Lists (
 	/* length 2 list ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
@@ -379,16 +386,9 @@ CREATE TABLE mydatabase.L4Lists (
 		element_1_type TINYINT,
 		element_2_type TINYINT,
 		element_3_type TINYINT,
-		element_4_type TINYINT,
+		element_4_type TINYINT
 	/**/
-
-	/* user (or bot) responsible for upload */
-	user BIGINT FOREIGN KEY,
-	
-	/* timestamp */
-	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- type code for L3List1: 44.
 
 -- saving larger lists for later.
 
@@ -396,12 +396,11 @@ CREATE TABLE mydatabase.L4Lists (
 
 
 
+-- insert some users
+INSERT INTO Users (public_encryption_key) VALUES (NULL);
+INSERT INTO Users () VALUES ();
+INSERT INTO Users () VALUES ();
 
-/*
-INSERT INTO mydatabase.Users (username, password)
-	VALUES ("user0", "pw0");
-INSERT INTO mydatabase.Users (username, password)
-	VALUES ("user1", "pw1");
-INSERT INTO mydatabase.Users (username, password)
-	VALUES ("user2", "pw2");
-*/
+-- --test timestamp
+-- DO SLEEP(1);
+-- UPDATE Users SET public_encryption_key = 0xAAAA WHERE id = 1;
