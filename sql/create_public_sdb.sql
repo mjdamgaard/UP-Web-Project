953b30a843c737c4dcf8c1d03b58eea5846c5bce
mjdamgaard <mjdamgaard@outlook.dk>
2023-02-17 19:24:32 +0100

    I keep using TEXTs (for now). I have defined DescriptionSchemas.

--- sql/create_public_sdb.sql
+++ sql/create_public_sdb.sql
@@ -1,19 +1,22 @@
 USE mydatabase;
 
--- DROP TABLE Users;
--- DROP TABLE SemanticInputs;
--- DROP TABLE Statements;
--- DROP TABLE DescribedTerms;
--- DROP TABLE TTexts;
--- DROP TABLE STexts;
--- DROP TABLE MTexts;
--- DROP TABLE TBlobs;
--- DROP TABLE SBlobs;
--- DROP TABLE MBlobs;
--- DROP TABLE L1Lists;
--- DROP TABLE L2Lists;
--- DROP TABLE L3Lists;
--- DROP TABLE L4Lists;
+DROP TABLE Users;
+DROP TABLE SemanticInputs;
+DROP TABLE Statements;
+DROP TABLE DescribedTerms;
+DROP TABLE DescriptionSchemas;
+-- DROP TABLE Strings;
+DROP TABLE TTexts;
+DROP TABLE STexts;
+DROP TABLE MTexts;
+-- DROP TABLE Binaries;
+DROP TABLE TBlobs;
+DROP TABLE SBlobs;
+DROP TABLE MBlobs;
+DROP TABLE L1Lists;
+DROP TABLE L2Lists;
+DROP TABLE L3Lists;
+DROP TABLE L4Lists;
 
 
 
@@ -52,11 +55,6 @@ CREATE TABLE SemanticInputs (
 
 		/* statement types */
 		-- allowed statement types: only Statement (so no flag needed).
-			-- OBSOLETE:
-			-- statement_type TINYINT CHECK (
-			-- 	statement_type = 1 OR -- (MonadicStatement)
-			-- 	statement_type = 2    -- (DyadicStatement)
-			-- ),
 
 		/* value types */
 			-- (OBSOLETE: allowed value types: only BIGINT (so no flag needed).)
@@ -91,47 +89,7 @@ CREATE TABLE Statements (
 			statement_type = 4    -- (FunctionalTerm)
 		)
 	/**/
-
-	-- /* user (or bot) responsible for upload */
-	-- user BIGINT,
-	--
-	-- /* timestamp */
-	-- createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-
-
-
--- CREATE TABLE DyadicStatements (
--- 	/* dyadic statement ID */
--- 	id BIGINT AUTO_INCREMENT,
--- 	PRIMARY KEY(id),
-
--- 	/* primary fields */
--- 	subject BIGINT,
--- 	relation BIGINT,
--- 	object BIGINT,
-
--- 	/* database types (tables) of primary fields */
--- 		/* subject types */
--- 		-- allowed subject types: any (so no constraints).
--- 		subject_type TINYINT,
-
--- 		/* predicate types */
--- 		-- allowed predicate types: only DescribedTerm (so no flag needed).
-
--- 		/* object types */
--- 		-- allowed object types: any (so no constraints).
--- 		object_type TINYINT,
--- 	/**/
-
--- 	/* user (or bot) responsible for upload */
--- 	user BIGINT,
-
--- 	/* timestamp */
--- 	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
-
 
 
 
@@ -144,58 +102,28 @@ CREATE TABLE DescribedTerms (
 	/* primary fields */
 	description_schema BIGINT,
 	schema_input BIGINT,
+	-- (these two fields combine into one "Description" of the Term in question)
 
 	/* database types (tables) of primary fields */
-		/* subject types */
-		-- allowed subject types: TText or SText.
-		description_schema_type TINYINT CHECK (
-			description_schema_type = 11 OR -- (TText)
-			description_schema_type = 12    -- (SText)
-		),
+		/* description_schema types */
+		-- allowed description_schema types: DescriptionSchema (no flag needed).
 
 		/* schema_input types */
 		-- allowed schema_input types: any (so no constraints).
 		schema_input_type TINYINT
 	/**/
-
-	-- /* user (or bot) responsible for upload */
-	-- user BIGINT,
-	--
-	-- /* timestamp */
-	-- createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
--- -- type code for FunctionalTerm: 4.
--- CREATE TABLE FunctionalTerms (
--- 	/* functional term ID */
--- 	id BIGINT AUTO_INCREMENT,
--- 	PRIMARY KEY(id),
---
--- 	/* primary fields */
--- 	function BIGINT,
--- 	input BIGINT,
---
--- 	/* database types (tables) of primary fields */
--- 		/* input types */
--- 		-- allowed input types: any (so no constraints).
--- 		subject_type TINYINT,
---
--- 		/* function types */
--- 		-- allowed function types: DescribedTerm or FunctionalTerm.
--- 		statement_type TINYINT CHECK (
--- 			statement_type = 3 OR -- (DescribedTerm)
--- 			statement_type = 4    -- (FunctionalTerm)
--- 		)
--- 	/**/
---
--- 	-- /* user (or bot) responsible for upload */
--- 	-- user BIGINT,
--- 	--
--- 	-- /* timestamp */
--- 	-- createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+-- type code for DescriptionSchemas: 4.
+CREATE TABLE DescriptionSchemas (
+	/* description schema ID */
+	id BIGINT AUTO_INCREMENT,
+	PRIMARY KEY(id),
 
+	/* data (text defining the description schema) */
+	str TEXT
+);
 
 
 -- type code for DateTime: 6.
@@ -212,19 +140,25 @@ CREATE TABLE DescribedTerms (
 
 
 
+-- -- type code for Strings: 20.
+-- CREATE TABLE Strings (
+-- 	/* string data entity ID */
+-- 	id BIGINT AUTO_INCREMENT,
+-- 	PRIMARY KEY(id),
+--
+-- 	/* data */
+-- 	str VARCHAR(16383)
+-- );
+
+
 -- type code for TText: 21.
 CREATE TABLE TTexts (
 	/* tiny text ID */
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
-	/* primary fields */
-	t_text TINYTEXT
-
-	/* database types (tables) of primary fields */
-		/* t_text types */
-		-- allowed t_text types: TINYTEXT (so no flag needed).
-	/**/
+	/* data */
+	str TINYTEXT
 );
 
 -- type code for SText: 22.
@@ -233,13 +167,8 @@ CREATE TABLE STexts (
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
-	/* primary fields */
-	s_text TEXT
-
-	/* database types (tables) of primary fields */
-		/* s_text types */
-		-- allowed s_text types: TEXT (so no flag needed).
-	/**/
+	/* data */
+	str TEXT
 );
 
 -- type code for MText: 23.
@@ -248,19 +177,25 @@ CREATE TABLE MTexts (
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
-	/* primary fields */
-	m_text MEDIUMTEXT
-
-	/* database types (tables) of primary fields */
-		/* m_text types */
-		-- allowed m_text types: MEDIUMTEXT (so no flag needed).
-	/**/
+	/* data */
+	str MEDIUMTEXT
 );
+
 
 -- saving LTexts for later.
 -- saving DeltaTexts and CompoundTexts for later.
 
 
+
+-- -- type code for Binary: 30.
+-- CREATE TABLE Binaries (
+-- 	/* string data entity ID */
+-- 	id BIGINT AUTO_INCREMENT,
+-- 	PRIMARY KEY(id),
+--
+-- 	/* data */
+-- 	bin VARBINARY(65535)
+-- );
 
 -- type code for TBlob: 31.
 CREATE TABLE TBlobs (
@@ -268,13 +203,8 @@ CREATE TABLE TBlobs (
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
-	/* primary fields */
-	t_blob TINYBLOB
-
-	/* database types (tables) of primary fields */
-		/* t_blob types */
-		-- allowed t_blob types: TINYBLOB (so no flag needed).
-	/**/
+	/* data */
+	bin TINYBLOB
 );
 
 -- type code for SBlob: 32.
@@ -283,13 +213,8 @@ CREATE TABLE SBlobs (
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
-	/* primary fields */
-	s_blob BLOB
-
-	/* database types (tables) of primary fields */
-		/* s_blob types */
-		-- allowed s_blob types: BLOB (so no flag needed).
-	/**/
+	/* data */
+	bin BLOB
 );
 
 -- type code for MBlob: 33.
@@ -298,13 +223,8 @@ CREATE TABLE MBlobs (
 	id BIGINT AUTO_INCREMENT,
 	PRIMARY KEY(id),
 
-	/* primary fields */
-	m_blob MEDIUMBLOB
-
-	/* database types (tables) of primary fields */
-		/* m_blob types */
-		-- allowed m_blob types: MEDIUMBLOB (so no flag needed).
-	/**/
+	/* data */
+	bin MEDIUMBLOB
 );
 
 -- saving LBlobs for later.
