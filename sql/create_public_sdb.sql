b3ff382716ee8110c0c414e1457b2b0a4d3abdeb
mjdamgaard <mjdamgaard@outlook.dk>
2023-02-18 18:31:16 +0100

    changed Predicates and DescribedEntities.

--- sql/create_public_sdb.sql
+++ sql/create_public_sdb.sql
@@ -22,142 +22,140 @@ DROP TABLE L4Lists;
 
 -- type code for User: 0.
 CREATE TABLE Users (
-	/* user ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* user ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* primary fields */
-	public_encryption_key BIGINT,
+    /* primary fields */
+    public_encryption_key BIGINT,
 
-	/* database types (tables) of primary fields */
-		/* public_encryption_key types */
-		-- allowed public_encryption_key types: TBLOB (so no flag needed).
-	/**/
+    /* database types (tables) of primary fields */
+        /* public_encryption_key types */
+        -- allowed public_encryption_key types: TBLOB (so no flag needed).
+    /**/
 
-	/* timestamp */
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    /* timestamp */
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- type code for SemanticInput: 1.
 CREATE TABLE SemanticInputs (
-	/* semantic input ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* semantic input ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* primary fields */
-	user BIGINT,
-	statement BIGINT,
-	value BIGINT,
+    /* primary fields */
+    user BIGINT,
+    statement BIGINT,
+    value BIGINT,
 
-	/* database types (tables) of primary fields */
-		/* user types */
-		-- allowed user types: only User (so no flag needed).
+    /* database types (tables) of primary fields */
+        /* user types */
+        -- allowed user types: only User (so no flag needed).
 
-		/* statement types */
-		-- allowed statement types: only Statement (so no flag needed).
+        /* statement types */
+        -- allowed statement types: only Statement (so no flag needed).
 
-		/* value types */
-			-- (OBSOLETE: allowed value types: only BIGINT (so no flag needed).)
-		-- allowed value types: any (so no constraints).
-		value_type TINYINT,
-	/**/
+        /* value types */
+            -- (OBSOLETE: allowed value types: only BIGINT (so no flag needed).)
+        -- allowed value types: any (so no constraints).
+        value_type TINYINT,
+    /**/
 
-	/* timestamp */
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    /* timestamp */
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
 -- type code for Statement: 2.
 CREATE TABLE Statements (
-	/* statement ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* statement ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* primary fields */
-	subject BIGINT,
-	predicate BIGINT,
+    /* primary fields */
+    subject BIGINT,
+    predicate BIGINT,
 
-	/* database types (tables) of primary fields */
-		/* subject types */
-		-- allowed subject types: any (so no constraints).
-		subject_type TINYINT
+    /* database types (tables) of primary fields */
+        /* subject types */
+        -- allowed subject types: any (so no constraints).
+        subject_type TINYINT
 
-		/* predicate types */
-			-- -- allowed predicate types: DescribedTerm or FunctionalTerm.
-			-- predicate_type TINYINT CHECK (
-			-- 	statement_type = 3 OR -- (DescribedTerm)
-			-- 	statement_type = 4    -- (FunctionalTerm)
-			-- )
-		-- allowed predicate types: Predicate (so no flag needed).
-	/**/
+        /* predicate types */
+        -- allowed predicate types: Predicate (so no flag needed).
+    /**/
 );
 
 
 
 -- type code for DescribedEntities: 3.
 CREATE TABLE DescribedEntity (
-	/* described term ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* described term ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* primary fields */
-	--TODO: Change!
-	description_schema BIGINT,
-	schema_input BIGINT,
-	-- (these two fields combine into one "Description" of the Term in question)
+    /* primary fields */
+    -- List of attributes (i.e. the attribute names), predicates, relations,
+    -- categories, etc., which all defines the overall type and category of the
+    -- entity.
+    categorizing_list BIGINT,
+    -- List of input specifically to each attribute and relation in the catego-
+    -- rizing list (with all other elements of that list ignored) in the same
+    -- order as how the attributes and relations appear in that list. 
+    input_list BIGINT,
 
-	/* database types (tables) of primary fields */
-		/* description_schema types */
-		-- allowed description_schema types: DescriptionSchema (no flag needed).
+    /* database types (tables) of primary fields */
+        /* categorizing_list types */
+        -- allowed categorizing_list types: any List types.
+        categorizing_list_type TINYINT CHECK (
+            categorizing_list_type >= 40 -- all List types
+        ),
 
-		/* schema_input types */
-		-- allowed schema_input types: any (so no constraints).
-		schema_input_type TINYINT
-	/**/
+        /* input_list types */
+        -- allowed input_list types: any List types.
+        input_list_type TINYINT CHECK (
+            input_list_type >= 40 -- all List types
+        )
+    /**/
 );
 
 
 -- -- type code for DescriptionSchemas: 4.
 -- CREATE TABLE DescriptionSchemas (
--- 	/* description schema ID */
--- 	id BIGINT AUTO_INCREMENT,
--- 	PRIMARY KEY(id),
+--     /* description schema ID */
+--     id BIGINT AUTO_INCREMENT,
+--     PRIMARY KEY(id),
 --
--- 	/* data (text defining the description schema) */
--- 	str TEXT
+--     /* data (text defining the description schema) */
+--     str TEXT
 -- );
 
 
 -- type code for Predicate: 4.
 CREATE TABLE Predicates (
-	/* predicate ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* predicate ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* primary fields */
-	relation BIGINT,
-	object BIGINT,
-	-- (these two fields combine into one "Description" of the Term in question)
+    /* primary fields */
+    rel_or_att BIGINT, -- relation or attribute depending on the database type.
+    inpput BIGINT, -- object of the realtion or value of the attribute.
+    -- (these two fields combine into one "Description" of the Term in question)
 
-	/* database types (tables) of primary fields */
-		/* relation types */
-		-- allowed relation types: any (so no constraints).
-		relation_type TINYINT,
+    /* database types (tables) of primary fields */
+        /* relation/attribute types */
+        -- allowed rel_or_att types: DescripedEntity (if rel) or TText (if att).
+        rel_or_att_type TINYINT CHECK (
+            rel_or_att_type = 3 OR -- DescripedEntity
+            rel_or_att_type = 21   -- TText
+        ),
 
-		/* object types */
-		-- allowed object types: any (so no constraints).
-		object_type TINYINT
-	/**/
+        /* inpput types */
+        -- allowed inpput types: any (so no constraints).
+        inpput_type TINYINT
+    /**/
 );
-
-
-
-
-
-
-
-
-
 
 
 
@@ -184,43 +182,43 @@ CREATE TABLE Predicates (
 
 -- -- type code for Strings: 20.
 -- CREATE TABLE Strings (
--- 	/* string data entity ID */
--- 	id BIGINT AUTO_INCREMENT,
--- 	PRIMARY KEY(id),
+--     /* string data entity ID */
+--     id BIGINT AUTO_INCREMENT,
+--     PRIMARY KEY(id),
 --
--- 	/* data */
--- 	str VARCHAR(16383)
+--     /* data */
+--     str VARCHAR(16383)
 -- );
 
 
 -- type code for TText: 21.
 CREATE TABLE TTexts (
-	/* tiny text ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* tiny text ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* data */
-	str TINYTEXT
+    /* data */
+    str TINYTEXT
 );
 
 -- type code for SText: 22.
 CREATE TABLE STexts (
-	/* small text ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* small text ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* data */
-	str TEXT
+    /* data */
+    str TEXT
 );
 
 -- type code for MText: 23.
 CREATE TABLE MTexts (
-	/* medium text ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* medium text ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* data */
-	str MEDIUMTEXT
+    /* data */
+    str MEDIUMTEXT
 );
 
 
@@ -231,42 +229,42 @@ CREATE TABLE MTexts (
 
 -- -- type code for Binary: 30.
 -- CREATE TABLE Binaries (
--- 	/* string data entity ID */
--- 	id BIGINT AUTO_INCREMENT,
--- 	PRIMARY KEY(id),
+--     /* string data entity ID */
+--     id BIGINT AUTO_INCREMENT,
+--     PRIMARY KEY(id),
 --
--- 	/* data */
--- 	bin VARBINARY(65535)
+--     /* data */
+--     bin VARBINARY(65535)
 -- );
 
 -- type code for TBlob: 31.
 CREATE TABLE TBlobs (
-	/* tiny BLOB ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* tiny BLOB ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* data */
-	bin TINYBLOB
+    /* data */
+    bin TINYBLOB
 );
 
 -- type code for SBlob: 32.
 CREATE TABLE SBlobs (
-	/* small BLOB ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* small BLOB ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* data */
-	bin BLOB
+    /* data */
+    bin BLOB
 );
 
 -- type code for MBlob: 33.
 CREATE TABLE MBlobs (
-	/* medium BLOB ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* medium BLOB ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* data */
-	bin MEDIUMBLOB
+    /* data */
+    bin MEDIUMBLOB
 );
 
 -- saving LBlobs for later.
@@ -278,78 +276,78 @@ CREATE TABLE MBlobs (
 
 -- type code for L1List: 41.
 CREATE TABLE L1Lists (
-	/* length 1 list ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* length 1 list ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* primary fields */
-	element_1 BIGINT,
+    /* primary fields */
+    element_1 BIGINT,
 
-	/* database types (tables) of primary fields */
-		/* element types */
-		-- allowed element types: any (so no constraints).
-		element_1_type TINYINT
-	/**/
+    /* database types (tables) of primary fields */
+        /* element types */
+        -- allowed element types: any (so no constraints).
+        element_1_type TINYINT
+    /**/
 );
 
 -- type code for L2List: 42.
 CREATE TABLE L2Lists (
-	/* length 2 list ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* length 2 list ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* primary fields */
-	element_1 BIGINT,
-	element_2 BIGINT,
+    /* primary fields */
+    element_1 BIGINT,
+    element_2 BIGINT,
 
-	/* database types (tables) of primary fields */
-		/* element types */
-		-- allowed element types: any (so no constraints).
-		element_1_type TINYINT,
-		element_2_type TINYINT
-	/**/
+    /* database types (tables) of primary fields */
+        /* element types */
+        -- allowed element types: any (so no constraints).
+        element_1_type TINYINT,
+        element_2_type TINYINT
+    /**/
 );
 
 -- type code for L3List: 43.
 CREATE TABLE L3Lists (
-	/* length 2 list ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* length 2 list ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* primary fields */
-	element_1 BIGINT,
-	element_2 BIGINT,
-	element_3 BIGINT,
+    /* primary fields */
+    element_1 BIGINT,
+    element_2 BIGINT,
+    element_3 BIGINT,
 
-	/* database types (tables) of primary fields */
-		/* element types */
-		-- allowed element types: any (so no constraints).
-		element_1_type TINYINT,
-		element_2_type TINYINT,
-		element_3_type TINYINT
-	/**/
+    /* database types (tables) of primary fields */
+        /* element types */
+        -- allowed element types: any (so no constraints).
+        element_1_type TINYINT,
+        element_2_type TINYINT,
+        element_3_type TINYINT
+    /**/
 );
 
 -- type code for L3List: 44.
 CREATE TABLE L4Lists (
-	/* length 2 list ID */
-	id BIGINT AUTO_INCREMENT,
-	PRIMARY KEY(id),
+    /* length 2 list ID */
+    id BIGINT AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
-	/* primary fields */
-	element_1 BIGINT,
-	element_2 BIGINT,
-	element_3 BIGINT,
-	element_4 BIGINT,
+    /* primary fields */
+    element_1 BIGINT,
+    element_2 BIGINT,
+    element_3 BIGINT,
+    element_4 BIGINT,
 
-	/* database types (tables) of primary fields */
-		/* element types */
-		-- allowed element types: any (so no constraints).
-		element_1_type TINYINT,
-		element_2_type TINYINT,
-		element_3_type TINYINT,
-		element_4_type TINYINT
-	/**/
+    /* database types (tables) of primary fields */
+        /* element types */
+        -- allowed element types: any (so no constraints).
+        element_1_type TINYINT,
+        element_2_type TINYINT,
+        element_3_type TINYINT,
+        element_4_type TINYINT
+    /**/
 );
 
 -- saving larger lists for later.
