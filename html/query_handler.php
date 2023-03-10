36513ae79db8a9bbe8d0dbb40126aec13ba9173c
mjdamgaard <mjdamgaard@outlook.dk>
2023-03-10 13:17:07 +0100

    before defining first request handling subprocedure (for getSet()). I should probably rename some files, though.

--- html/query_handler.php
+++ html/query_handler.php
@@ -1,22 +1,44 @@
 <?php
 
-$subroutines_path = $_SERVER['DOCUMENT_ROOT'] . "/src/subroutines/";
+$src_path = $_SERVER['DOCUMENT_ROOT'] . "/src/";
+require_once $src_path . "query_lib_0_00.php";
 
 
-// check that http method is the POST method and get $protocol variable.
-$protocol = "";
-require $subroutines_path . "get_and_check_protocol_from_post.php";
+// $subroutines_path = $_SERVER['DOCUMENT_ROOT'] . "/src/subroutines/";
+//
+// // check that http method is the POST method and get $protocol variable.
+// $protocol = "";
+// require $subroutines_path . "get_and_check_protocol_from_post.php";
 
 
-// NOTE: this protocol requires no user authenication.
+// check that http method is the POST method.
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+   echo "Error: Only the POST method is implemented";
+   exit;
+}
 
-// get request type.
-$reqType = "";
-require $subroutines_path . "get_request_type.php";
+// get the protocol.
+if (!isset($_POST["p"])) {
+   echo "Error: No protocol specified";
+   exit;
+}
+$protocol = intval($_POST["p"]);
 
 
-
-
+// match with known protocols.
+switch ($protocol) {
+    case "0.0":
+        // TODO: Discontinue this protocol.
+        safeEcho(p_0_00\getRawQueryResult());
+        break;
+    case "1.0":
+        // TODO: Implement "insert/input term" protocol/procedure.
+        echo "Error: Protocol not implemented yet";
+        exit;
+        break;
+    default:
+        echo "Error: Unrecognized protocol";
+        exit;
 }
 
 
@@ -24,15 +46,12 @@ require $subroutines_path . "get_request_type.php";
 
 
 
+function safeEcho($str) {
+    echo convertToSafeOutputFormat($str);
+}
 
-
-
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+function convertToSafeOutputFormat($str) {
+    return htmlspecialchars(stripslashes($str));
 }
 
 
