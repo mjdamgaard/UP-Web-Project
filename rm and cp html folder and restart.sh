629d563e350c226beb2f313a2906a49891c9213c
mjdamgaard <mjdamgaard@outlook.dk>
2023-03-12 09:07:00 +0100

    moving src/ out of html/ (and just adding path to the bash program instead).

--- rm and cp html folder and restart.sh
+++ rm and cp html folder and restart.sh
@@ -1,3 +1,5 @@
 sudo rm -r /var/www/html/*
-sudo cp -r /home/madsjd/GitHub/SemDB_prototyping//html /var/www/
+sudo cp -r /home/madsjd/GitHub/SemDB_prototyping/html /var/www/
+sudo rm -r /var/www/src/*
+sudo cp -r /home/madsjd/GitHub/SemDB_prototyping/src /var/www/
 sudo /etc/init.d/apache2 restart
