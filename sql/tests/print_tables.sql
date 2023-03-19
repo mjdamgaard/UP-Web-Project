3ec06457127495bc8da2956622902e20b3946bed
mjdamgaard <mjdamgaard@outlook.dk>
2023-03-19 16:58:04 +0100

    f* it, I will actually make the MySQL procedures receive and return hexadecimals for binary data (i.e. ratingVals) as well..

--- sql/tests/print_tables.sql
+++ sql/tests/print_tables.sql
@@ -27,32 +27,51 @@ SELECT
 FROM SemanticInputs
 ORDER BY set_id ASC, rat_val DESC;
 
-
-/* MySQL binary handling hack test */
-
--- MySQL has wierd behaviour here:
-SELECT CONV(0x0F, 10, 16); -- = "F"; OK.
-SELECT UNHEX("F"); -- = 0x0F; OK.
-SELECT CONV(UNHEX("F"), 10, 16); -- = 0; not OK..!
-
--- Let us try something else..
-
-DELIMITER //
-CREATE PROCEDURE test ()
-BEGIN
-    DECLARE bin VARBINARY(255);
-    DECLARE str VARCHAR(510);
-    DECLARE n BIGINT UNSIGNED;
-    SET bin = UNHEX("100001");
-    SELECT HEX(bin);
-    SET str = HEX(bin);
-    -- SELECT CONV(bin, 10, 16); and SELECT CONV(bin, 2, 16); returns "0".
-    SET n = CONV(str, 16, 10);
-    SELECT n;
-END //
-DELIMITER ;
--- This does work, and I almost expect that UNHEX and HEX will cancel, but it
--- might depend on their implementation..
-
-CALL test ();
-DROP PROCEDURE test;
+--
+-- /* MySQL binary handling hack test */
+--
+-- -- MySQL has wierd behaviour here:
+-- SELECT CONV(0x0F, 10, 16); -- = "F"; OK.
+-- SELECT UNHEX("F"); -- = 0x0F; OK.
+-- SELECT CONV(UNHEX("F"), 10, 16); -- = 0; not OK..!
+--
+-- -- Let us try something else..
+--
+-- DELIMITER //
+-- CREATE PROCEDURE test ()
+-- BEGIN
+--     DECLARE bin VARBINARY(255);
+--     DECLARE str VARCHAR(510);
+--     DECLARE n BIGINT UNSIGNED;
+--     SET bin = UNHEX("100001");
+--     SELECT HEX(bin);
+--     SET str = HEX(bin);
+--     -- SELECT CONV(bin, 10, 16); and SELECT CONV(bin, 2, 16); returns "0".
+--     SET n = CONV(str, 16, 10);
+--     SELECT n;
+-- END //
+-- DELIMITER ;
+-- CALL test ();
+-- DROP PROCEDURE test;
+-- -- This does work, and I almost expect that UNHEX and HEX will cancel, but it
+-- -- might depend on their implementation.. ..Ah, let me also try CONV()..
+-- -- *Yeah, they probably don't necessarily cancel if they are both implemented
+-- -- as loops, which they most likely are; what else?.
+--
+-- -- DELIMITER //
+-- -- CREATE PROCEDURE test ()
+-- -- BEGIN
+-- --     DECLARE bin VARBINARY(255);
+-- --     DECLARE str VARCHAR(510);
+-- --     DECLARE n BIGINT UNSIGNED;
+-- --     SET bin = UNHEX("100001");
+-- --     SELECT CONV(bin, 2, 16); -- CONV(bin, 10, 16);
+-- --     SET str = CONV(bin, 10, 16);
+-- --     -- SELECT CONV(bin, 10, 16); and SELECT CONV(bin, 2, 16); returns "0".
+-- --     SET n = CONV(str, 16, 10);
+-- --     SELECT n;
+-- -- END //
+-- -- DELIMITER ;
+-- -- CALL test ();
+-- -- DROP PROCEDURE test;
+-- -- -- ..That doesn't work..
