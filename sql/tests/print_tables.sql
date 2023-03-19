a69d172ceb6e91e323034603a5da6685d797fbf8
mjdamgaard <mjdamgaard@outlook.dk>
2023-03-19 12:53:44 +0100

    oh no, I just need to not overwrite a column name with an identical alias before ordering.

--- sql/tests/print_tables.sql
+++ sql/tests/print_tables.sql
@@ -2,27 +2,21 @@
 
 -- SELECT "Categories:";
 SELECT
-    HEX(id) AS id,
+    HEX(id) AS catID,
     title,
     super_cat_id AS superCatID
-FROM (
-    SELECT * FROM Categories
-    ORDER BY id
-)
-AS Categories;
+FROM Categories
+ORDER BY id;
 
 
--- SELECT "Sets:";
 SELECT
     HEX(id) AS id,
     user_t, Hex(user_id) AS userID,
     subj_t, HEX(subj_id) AS subjID,
     HEX(rel_id) AS relID
-FROM (
-    SELECT * FROM Sets
-    ORDER BY user_t, user_id, subj_t, subj_id, rel_id
-)
-AS Sets;
+FROM Sets
+ORDER BY user_t, user_id, subj_t, subj_id, rel_id;
+
 
 -- SELECT "SemanticInputs:";
 SELECT
@@ -30,8 +24,5 @@ SELECT
     rat_val,
     obj_t,
     HEX(obj_id) AS objID
-FROM (
-    SELECT * FROM SemanticInputs
-    ORDER BY set_id ASC, rat_val DESC
-)
-AS SemanticInputs;
+FROM SemanticInputs
+ORDER BY set_id ASC, rat_val DESC;
