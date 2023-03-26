24cc973f8756773fab7653c290b3bd3cd0aaa883
mjdamgaard <mjdamgaard@outlook.dk>
2023-03-26 11:29:05 +0200

    removing parseMainScript() again.

--- html/src/user_scripts/productions/script.js
+++ html/src/user_scripts/productions/script.js
@@ -9,6 +9,17 @@ export function parseModuleScript(script) {
     // initialize position object and ...
     var nextPosObj = {pos:0};
 
+    // if first lexeme is '"use strict"', continue, or else return false.
+    if (lexArr[0] == '"use strict"' && lexArr[1] == ';') {
+        nextPosObj.pos = 2;
+    } else {
+        return false;
+    }
+    // (Even though strict mode is automatic for all modules, let us still
+    // include it just in case we allow other script types in the future, and
+    // also just in case sloppy mode can somehow make the scripts unsafe when
+    // not loaded as module scripts like they are supposed to.)
+
     // parse a list of import statements.
     var matchWasFound = true;
     while (matchWasFound) {
@@ -33,39 +44,42 @@ export function parseModuleScript(script) {
 }
 
 
-function parseMainScript(script) {
-    // get lexeme array.
-    // TODO: implement exception handling here.
-    var lexArr = lex(script);
-    // initialize position object and ...
-    var nextPosObj = {pos:0};
-
-    // if first lexeme is '"use strict"', continue, or else return false.
-    if (lexArr[0] == '"use strict"' && lexArr[1] == ';') {
-        nextPosObj.pos = 2;
-    } else {
-        return false;
-    }
-
-    // parse a list of import statements.
-    var matchWasFound = true;
-    while (matchWasFound) {
-        matchWasFound = parseImportStmt(lexArr, nextPosObj);
-    }
-
-    // parse a list of function definitions or pure variable definitions.
-    var matchWasFound = true;
-    while (matchWasFound) {
-        matchWasFound = parseStmt(lexArr, nextPosObj);
-    }
-
-    // return true only if all lexemes have been successfully parsed.
-    if (nextPosObj.pos == lexArr.length) {
-        return true;
-    } else {
-        return false;
-    }
-}
+/* Not needed after all, for users should instead define a main procedure
+ * in their main module.
+ **/
+// function parseMainScript(script) {
+//     // get lexeme array.
+//     // TODO: implement exception handling here.
+//     var lexArr = lex(script);
+//     // initialize position object and ...
+//     var nextPosObj = {pos:0};
+//
+//     // if first lexeme is '"use strict"', continue, or else return false.
+//     if (lexArr[0] == '"use strict"' && lexArr[1] == ';') {
+//         nextPosObj.pos = 2;
+//     } else {
+//         return false;
+//     }
+//
+//     // parse a list of import statements.
+//     var matchWasFound = true;
+//     while (matchWasFound) {
+//         matchWasFound = parseImportStmt(lexArr, nextPosObj);
+//     }
+//
+//     // parse a list of function definitions or pure variable definitions.
+//     var matchWasFound = true;
+//     while (matchWasFound) {
+//         matchWasFound = parseStmt(lexArr, nextPosObj);
+//     }
+//
+//     // return true only if all lexemes have been successfully parsed.
+//     if (nextPosObj.pos == lexArr.length) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
 
 
