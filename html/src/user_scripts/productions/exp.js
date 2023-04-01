67acec9538b233cbe9c231a0e3c9b726d6299944
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-01 13:59:16 +0200

    moving large comment (explaining the absence of the '.' operator) down to the end.

--- html/src/user_scripts/productions/exp.js
+++ html/src/user_scripts/productions/exp.js
@@ -12,59 +12,6 @@ class ParseException {
     }
 }
 
-// class VarType {
-//     constructor(typeClass, retOrElemType) {
-//         typeClass
-//     }
-// }
-// No, I'm just gonna use a string with a series of [(val)(arr)(obj)(fun)]
-// kaywords. (And I will use "get" instead of "undefined"..)
-// ...No, I can just use the fact that I am already using an array and just
-// let the next elements denote the return or element type.. or in fact, let
-// me reverse the order such that I for instance can pop "fun" from the array
-// and then know that the remaining array contains the return type.
-// No, that doesn't quite work for me, so I'll go back to using that string
-// format as my varType data structure..
-// ... Wait, why did I include all these types again, wasn't just because I
-// didn't want users to be able to.. Ah, right, to set the properties of
-// objects.. ..Hm, but can't I just simply.. Hm.. ..Oh! Because objects
-// parameters are not pointers, but are only called by reference so far as
-// to keep any changes you make to their elements or properties, then I must
-// simply be able to just limit how properties can be set/changed in this
-// language subset, and we're good!.. Hm, all that work, but whatever.. Yeah,
-// let me take a break and think about it a bit more, but yeah, I think I can
-// just make sure to limit objects changes to function calls, and then I should
-// be good, at least if I don't include any of those "+="-like assign operators,
-// that I don't really care to test for objects..
-// ... Nah, I don't really trust that people can't just build.. ..function
-// objects.. but if the properties cannot be set or changed except.. ..and
-// if only some identifiers are allowed as properties.. Maybe I should think
-// a bit more about it..
-// ... ... Can I not just have two types: functions and non-functions..?
-// ..Yes, that is probably, what I will do.. Should I then even transform
-// non-function variable names, or..? ..No, I shouldn't need to..
-// (01.04.23, 11:30) Getters and setters seem dangerous, but since I don't
-// include those keywords, their should be no way to initialize ones, right..?
-// Ah, and I should prevent "get" and "set" as a function identifier, but that
-// is done by always having a prefix on all identifiers (which I should have).
-// ...On second thought, maybe I should actually not include direct accessing
-// of object properties (via the '.' syntax) due to those getters and setters
-// of JS. Cause otherwise it wouldn't be a simple matter to say that only legal
-// function are ever called, cause then you have prove that no getters and
-// setters can be initialized via the allowed operators.. which is probably a
-// very easy thing to do if I had a more thorough JS documentation.. ..Well,
-// yeah, but the point is, even if it is easy to prove, you would still have
-// to reference the standard and walk a reader through the proof.. Hm, unless
-// the standard is very clear on the topic, let me see.. ..Ah, never mind. It
-// is just much much simpler to remove the '.'. And doing so also removes the
-// risk that the prefix I chose collides with some object property.. although,
-// I could just make sure to.. Oh no, cause object prototypes might have hidden
-// properties for all I know.. Ah, probably not, but anyway, it doesn't matter
-// now. I'll remove the '.' operator. It is not that handy anyway, now that
-// the users (in the beginning) have to add the prefixes to the property names.
-// Then using functions to get and set properties might be at least as good
-// in terms of aesthetics and clarity. (12:07)
-
 
 
 
@@ -280,3 +227,59 @@ function parseAssignOp(lexArr, nextPos, successRequired) {
     }
     return ret;
 }
+
+
+
+
+// class VarType {
+//     constructor(typeClass, retOrElemType) {
+//         typeClass
+//     }
+// }
+// No, I'm just gonna use a string with a series of [(val)(arr)(obj)(fun)]
+// kaywords. (And I will use "get" instead of "undefined"..)
+// ...No, I can just use the fact that I am already using an array and just
+// let the next elements denote the return or element type.. or in fact, let
+// me reverse the order such that I for instance can pop "fun" from the array
+// and then know that the remaining array contains the return type.
+// No, that doesn't quite work for me, so I'll go back to using that string
+// format as my varType data structure..
+// ... Wait, why did I include all these types again, wasn't just because I
+// didn't want users to be able to.. Ah, right, to set the properties of
+// objects.. ..Hm, but can't I just simply.. Hm.. ..Oh! Because objects
+// parameters are not pointers, but are only called by reference so far as
+// to keep any changes you make to their elements or properties, then I must
+// simply be able to just limit how properties can be set/changed in this
+// language subset, and we're good!.. Hm, all that work, but whatever.. Yeah,
+// let me take a break and think about it a bit more, but yeah, I think I can
+// just make sure to limit objects changes to function calls, and then I should
+// be good, at least if I don't include any of those "+="-like assign operators,
+// that I don't really care to test for objects..
+// ... Nah, I don't really trust that people can't just build.. ..function
+// objects.. but if the properties cannot be set or changed except.. ..and
+// if only some identifiers are allowed as properties.. Maybe I should think
+// a bit more about it..
+// ... ... Can I not just have two types: functions and non-functions..?
+// ..Yes, that is probably, what I will do.. Should I then even transform
+// non-function variable names, or..? ..No, I shouldn't need to..
+// (01.04.23, 11:30) Getters and setters seem dangerous, but since I don't
+// include those keywords, their should be no way to initialize ones, right..?
+// Ah, and I should prevent "get" and "set" as a function identifier, but that
+// is done by always having a prefix on all identifiers (which I should have).
+// ...On second thought, maybe I should actually not include direct accessing
+// of object properties (via the '.' syntax) due to those getters and setters
+// of JS. Cause otherwise it wouldn't be a simple matter to say that only legal
+// function are ever called, cause then you have prove that no getters and
+// setters can be initialized via the allowed operators.. which is probably a
+// very easy thing to do if I had a more thorough JS documentation.. ..Well,
+// yeah, but the point is, even if it is easy to prove, you would still have
+// to reference the standard and walk a reader through the proof.. Hm, unless
+// the standard is very clear on the topic, let me see.. ..Ah, never mind. It
+// is just much much simpler to remove the '.'. And doing so also removes the
+// risk that the prefix I chose collides with some object property.. although,
+// I could just make sure to.. Oh no, cause object prototypes might have hidden
+// properties for all I know.. Ah, probably not, but anyway, it doesn't matter
+// now. I'll remove the '.' operator. It is not that handy anyway, now that
+// the users (in the beginning) have to add the prefixes to the property names.
+// Then using functions to get and set properties might be at least as good
+// in terms of aesthetics and clarity. (12:07)
