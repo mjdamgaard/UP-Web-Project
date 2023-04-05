c2be9afdb474bb2adb76256960d2d277854e7381
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-05 20:53:02 +0200

    replacing all that with just a function for developers to get user-defined funtions directly from window.

--- UPA_cached_modules/dev_modules/t3.js
+++ UPA_cached_modules/dev_modules/t3.js
@@ -1,47 +1,21 @@
 
-/* Some functions to cache and select callback functions */
+/* Some functions to get and ... callback functions */
 
-
-class FunctionCache {
-    public static cache = {};
-}
-
-
-const funKeyPattern = "/^\w+$/";
-
-
-export function upaFun_isValidFunKey(key) {
-    return key.test(funKeyPattern);
-}
-
-export function upaFun_assertValidFunKey(key) {
-    if (!upaFun_isValidFunKey(key)) {
-        throw new Exception(
-            "assertValidFunKey(): input is not a valid /^\w+$/ key"
-        );
-    }
-}
 
 // Note that since this function does not have the upaFun_ prefix, it cannot
 // be exported to the final user modules (but only to other developer modules).
-export function getCachedFunction(key) {
-    upaFun_assertValidFunKey(key)
-    return FunctionCache.cache[key];
-}
-
-export function upaFun_cacheFunction(funName, key) {
-    upaFun_assertValidFunKey(key);
+export function verifyAndGetUPAFunction(funName) {
     if (!funName.test("/^[\$\w]+$/")) {
         throw new Exception(
-            "cacheFunction(): function name is not a valid /^[\$\w]+$/ string"
+            "getUPAFunction(): function name is not a valid /^[\$\w]+$/ string"
         );
     }
     let fullFunName = "upaFun_" + funName;
     if (typeof window[fullFunName] != "function") {
         throw new Exception(
-            "cacheFunction(): function " + fullFunName +
+            "verifyAndGetUPAFunction(): function " + fullFunName +
                 " is not defined yet"
         );
     }
-    FunctionCache.cache[key] = window[fullFunName];
+    return window[fullFunName];
 }
