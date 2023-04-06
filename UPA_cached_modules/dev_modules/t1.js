8e6f4346faffe46bafe59fac968dab562eedc4a1
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-06 12:47:40 +0200

    re-implementing getJQueryObj() using jQuery.find().

--- UPA_cached_modules/dev_modules/t1.js
+++ UPA_cached_modules/dev_modules/t1.js
@@ -2,108 +2,35 @@
 /* Some functions to cache and select jQuery objects */
 
 
+var mainFrameJQueryObj = $("#upaMainFrame");
 
 class JQueryObjCache {
-    // public static cache = {"mainFrame":$("#upaMainFrame")};
-    public static cache = {};
+    public static cache = {"mainFrame":mainFrameJQueryObj};
+    // public static cache = {};
 }
 
-
-export const elementNames =
-    [ +
-        "address", "article", "aside", "footer", "header", "h[1-6]",
-        "hgroup", "main", "nav", "section", "blockquote", "dd",
-        "div", "dl", "dt", "figcaption", "figure", "hr",
-        "li", "menu", "ol", "p", "pre", "ul",
-        "a", "abbr", "b", "bdi", "bdo", // "br" not included
-        "cite", "code", "data", "dfn", "em", "i",
-        // TODO: complete this.
-        "aaa", "aaa", "aaa", "aaa", "aaa", "aaa",
-        "aaa", "aaa", "aaa", "aaa", "aaa", "aaa",
-        "aaa", "aaa", "aaa", "aaa", "aaa", "aaa",
-    ];
-
-export const typeSelectorPattern =
-    "((" +
-        elementNames.join(")|(") +
-    "))";
-
-export const pseudoClasses =
-    [ +
-        "first", "last", "even", "odd",
-        "[(first)(last)(only)]\-[(child)(of\-type)]",
-        "nth\-(last\-)?[(child)(of\-type)]\([1-9][0-9]*\)",
-        "eq\((0|[1-9][0-9]*)\)",
-        "[(gt)(lt)]\(([1-9][0-9]*)\)",
-        "header", "animated", "focus", "empty", "parent", "hidden",
-        "visible", "input", "text", "password", "radio", "checkbox",
-        "submit", "reset", "button", "image", "file",
-        "enabled", "diabled", "selected", "checked",
-        "lang\(\w+(\-\w+)*\)",
-    ];
-
-export const pseudoClassPattern =
-    "((" +
-        pseudoClasses.join(")|(") +
-    "))";
-
-const attSelectorPattern = "(\[" + "\w+" + "([!\$\|\^~\*]?=\w+)?" + "\])";
-
-const combinatorPattern = "[ >~\+(\|\|)]";
-
-const selectorPattern1 =
-    "((" +
-        "\*" +
-    ")|("
-        typeSelectorPattern + "?" + attSelectorPattern + "*" +
-    "))";
-
-const selectorPattern2 = selectorPattern1 ...
-
-export const selectorPattern =
-    "/^((" +
-        "\$\w+" +
-    ")|("
-        "[(div)(a)(p)(#\w+)(\.\w+)]?" + "(\[\w+(=\w+)?\])?" + "([ >][\*])?" +
-    "))$/";
-
-
-
-
-export function upaFun_isValidSelector(selector) {
-    // if (typeof selector !== "string") {
-    //     throw new Exception("isValidSelector(): selector is not a string");
-    // }
-    return selector.test(selectorPattern);
-}
-
-export function upaFun_assertValidSelector(selector) {
-    if (!upaFun_isValidSelector(selector)) {
-        throw new Exception(
-            "assertValidSelector(): input is not a valid selector"
-        );
-    }
-}
-
-function getAbsoluteSelector(selector) {
-    // TODO: Change if/when selectors can include commas (unions).
-    return "#upaMainFrame " + selector;
-}
 
 // Note that since this function does not have the upaFun_ prefix, it cannot
 // be exported to the final user modules (but only to other developer modules).
-export function verifySelectorAndGetJQueryObj(selector) {
-    upaFun_assertValidSelector(selector);
-    if (selector.test("/^\$/")) {
+export function getJQueryObj(selector) {
+    if (typeof selector !== "string") {
+        throw new Exception(
+            "getJQueryObj(): input selector is not a string"
+        );
+    }
+    if (selector.test("/^\$\w+$/")) {
         return JQueryObjCache.cache[substring(selector, 1)];
     } else {
-        return $(getAbsoluteSelector(selector));
+        // TODO: Test/check that jQuery.find() is safe for any string input
+        // such that it always returns a descendent element or null/undefined
+        // or throws an exception.
+        return mainFrameJQueryObj.find(selector);
     }
 }
 
 
 export function upaFun_cacheJQueryObj(selector, key) {
-    let jqObj = verifySelectorAndGetJQueryObj(selector);
+    let jqObj = getJQueryObj(selector);
     if (!key.test("/^\w+$/")) {
         throw new Exception(
             "cacheJQueryObj(): input key is not a valid /^\w+$/ string"
