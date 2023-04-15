3f600c5fe714e2c1c3afd29fa8ca85ef216f1052
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-15 14:07:42 +0200

    in the middle of some changes..

--- UPA_cached_modules/dev_modules/t1.js
+++ UPA_cached_modules/dev_modules/t1.js
@@ -276,76 +276,6 @@ export function upaf_getAttributes(selector, keyArr) {
 
 
 
-/* Functions to set and get the types of <input> HTML elements */
-
-export const legalInputTypes [
-    "button", "checkbox", "color", "date", "file", "hidden", "image",
-    "month", "number", "radio", "range", "reset", "search", "submit",
-    "tel", "text", "time", "url", "week",
-];
-
-export function upaf_setInputType(selector, type) {
-    let jqObj = getJQueryObj(selector);
-    // test type
-    if (!(type in legalInputTypes)) {
-        throw new Exception(
-            "setInputType(): invalid or illegal type"
-        );
-    }
-    // set the input types of all selected <input> elements.
-    jqObj.filter('input').attr("type", type);
-}
-
-export function upaf_getInputType(selector) {
-    let jqObj = getJQueryObj(selector);
-    // return the input type of the first <input> element in the selection.
-    return jqObj.filter('input').attr("type");
-}
-
-// TODO: Add some more functions to set input attributes, such as 'pattern',
-// 'placeholder' and 'list'..
-
-
-
-
-/* Functions to set form actions (to javascript functions) */
-
-// Note that upaf_setFormAction(<selector>, "void") is useful to call even
-// when submission is set to be almost entirely handled by a jQuery event
-// instead.
-export function upaf_setFormAction(selector, funName) {
-    let jqObj = getJQueryObj(selector);
-    // test funName.
-    if (!/^[\$\w]+$/.test(funName)) {
-        throw new Exception(
-            "setFormAction(): function name is not a valid " +
-            "/^[\\$\\w]+$/ string"
-        );
-    }
-    // if "void" is given as the "funName," set the action attributes for all
-    // selected <form> elements to "javascript:void(0)".
-    if (funName === "void") {
-        jqObj.filter('form').attr("action", "javascript:void(0)");
-    // else set the action attributes to "javascript:upaf_<funName>(<id>),"
-    // where id is either the id of the <form> element, or is "" if the
-    // element does not have any id set.
-    } else {
-        let actionStart = "javascript:upaf_" + funName + "(";
-        jqObj.filter('form').each(function(){
-            var id = "";
-            if (typeof this[0].id === "string") {
-                id = this[0].id.substring(5); // i.e. without the upai_ prefix.
-            }
-            this.attr("action", actionStart + id + ")");
-        });
-    }
-}
-
-
-
-
-// TODO: Potentially add some more functions to set HTML attributes..
-
 
 
 
@@ -358,6 +288,8 @@ export const legalInputTypes [
     "tel", "text", "time", "url", "week",
 ];
 
+export const legalFormActionRegEx =
+    /^javascript:[(void\(0\))(upaf_[\$\w]+\(\w*\))]$/;
 
 // TODO: Find out where to place this function.
 export function upaf_isLegalKeyValAttrPair(tagName, key, val) {
@@ -366,16 +298,68 @@ export function upaf_isLegalKeyValAttrPair(tagName, key, val) {
             switch (tagName) {
                 case "input":
                     return legalInputTypes.includes(val);
+                default:
+                    return false;
+            }
+            break;
+        case "action":
+            switch (tagName) {
+                case "form":
+                    return legalFormActionRegEx.test(val);
+                default:
+                    return false;
             }
             break;
         default:
             return false;
     }
 }
-// TODO: Add some more functions to set input attributes, such as 'pattern',
-// 'placeholder' and 'list'..
+// TODO: Add some more attributes, such as 'pattern', 'placeholder' and 'list'..
 
 
+export function setAttributeOfJQueryObj(jqObj, tagName, key, val) {
+    // verify that key and val are defined and have the right
+    // formats.
+    if (!attrKeyRegEx.test(key)) {
+        throw new Exception(
+            "setAttributeOfJQueryObj(): input contains an invalid " +
+            "attribute key"
+        );
+    }
+    if (!attrValRegEx.test(val)) {
+        throw new Exception(
+            "setAttributeOfJQueryObj(): input contains an invalid " +
+            "attribute value"
+        );
+    }
+    // if attribute key starts with '~,' set a new upaa_ attribute
+    // for the new HTML element.
+    if (key.substring(0, 1) === "~") {
+        jqObj.attr("upaa_" + key.substring(1), val);
+    // if it is instead an id, test that it has not already been
+    // used and make sure to record it. (Let's not care about
+    // race conditions.)
+    } else if (key === "id") {
+        if (upaf_isExistingID(val)) {
+            throw new Exception(
+                "getHTML(): id=\"" + val +
+                "\" has already been used"
+            );
+        }
+        upaf_recordID(val);
+        jqObj.attr("id", val);
+    // else, test that it is one of the allowed nagName--key--val
+    // tuples.
+    } else if (upaf_isLegalKeyValAttrPair(tagName, key, val)) {
+        jqObj.attr(key, val);
+    } else {
+        throw new Exception(
+            "setAttributeOfJQueryObj(): illegal combination of tagName, " +
+            "attribute key and attribute value at " +
+            "input[" + i.toString() + "][1][" + j.toString() + "]"
+        );
+    }
+}
 
 
 
@@ -401,23 +385,19 @@ export function upaf_isLegalKeyValAttrPair(tagName, key, val) {
  **/
 export function upaf_addHTML(selector, method, struct) {
     let jqObj = getJQueryObj(selector);
-    let html = upaf_getHTMLFromStructure(struct);
-    switch (method) {
-        case "append":
-        case "prepend":
-        case "before":
-        case "after":
-            return jqObj[method](html);
-            break;
-        default:
-            throw new Exception(
-                "addHTML(): method name not recognized"
-            );
+    // test method.
+    if (!["append", "prepend", "before", "after"].includes(method)) {
+        throw new Exception(
+            "addHTML(): method name not recognized"
+        );
     }
+    let html = getHTMLFromStructureAndRecordIDs(struct);
+    // insert html via the append(), prepend(), before() or after() method.
+    return jqObj[method](html);
 }
 
 
-export function upaf_getHTMLFromStructure(struct) {
+export function getHTMLFromStructureAndRecordIDs(struct) {
     // if struct is a string, return the converted (HTML safe) string.
     if (typeof struct === "string") {
         return upaf_convertHTMLSpecialChars(struct);
@@ -450,7 +430,7 @@ export function upaf_getHTMLFromStructure(struct) {
         // if tag input is undefined, simply append the converted content to
         // ret.
         if (typeof tagName === "undefined") {
-            ret = ret + upaf_getHTMLFromStructure(content);
+            ret = ret + getHTMLFromStructureAndRecordIDs(content);
             continue;
         }
         // else, test tag input.
@@ -460,7 +440,7 @@ export function upaf_getHTMLFromStructure(struct) {
             );
         }
         // initialize new HTML element.
-        var htmlElem = document.createElement(tagName);
+        var htmlElem = $("<" + tagName + "></" + tagName + ">");
         // test each attribute input and add the attribute key--value pair to
         // the new HTML element.
         if (typeof attributes !== "undefined") {
@@ -484,7 +464,10 @@ export function upaf_getHTMLFromStructure(struct) {
                 // if attribute key starts with '~,' set a new upaa_ attribute
                 // for the new HTML element.
                 if (key.substring(0, 1) === "~") {
-                    htmlElem.setAttribute("upaa_" + key.substring(1), val);
+                    htmlElem.attr("upaa_" + key.substring(1), val);
+                // if it is instead an id, test that it has not already been
+                // used and make sure to record it. (Let's not care about
+                // race conditions.)
                 } else if (key === "id") {
                     if (upaf_isExistingID(val)) {
                         throw new Exception(
@@ -493,9 +476,11 @@ export function upaf_getHTMLFromStructure(struct) {
                         );
                     }
                     upaf_recordID(val);
-                    htmlElem.setAttribute("id", val);
+                    htmlElem.attr("id", val);
+                // else, test that it is one of the allowed nagName--key--val
+                // tuples.
                 } else if (upaf_isLegalKeyValAttrPair(tagName, key, val)) {
-                    htmlElem.setAttribute(key, val);
+                    htmlElem.attr(key, val);
                 } else {
                     throw new Exception(
                         "getHTML(): illegal combination of tagName, " +
@@ -506,10 +491,9 @@ export function upaf_getHTMLFromStructure(struct) {
             }
         }
         // test and convert content, and add it inside the new HTML element.
-        // (This might not work in some Internet Explorer browsers, I think..)
-        htmlElem.innerHTML = upaf_getHTMLFromStructure(content);
+        htmlElem.html(getHTMLFromStructureAndRecordIDs(content));
         // append the new HTML element to ret.
-        ret = ret + htmlElem.outerHTML;
+        ret = ret + htmlElem[0].outerHTML;
     }
     // return the resulting HTML string.
     return ret;
