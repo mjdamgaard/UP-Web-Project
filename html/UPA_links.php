c775f41b51050e50ca519267feb19b5068b597ad
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-16 12:25:37 +0200

    how do we do that..

--- html/UPA_links.php
+++ html/UPA_links.php
@@ -3,14 +3,19 @@
 /* At this point (and perhaps it will remain that way), this server program
  * is suppoesed to be called upaf_cacheURLRegEx() specifically, located in
  * t4.js in the moment of writing. The responsibility of this program is then
- * to first of all look up a setID and potentially a patID for the given user,
- * meaning that if the user has rated a setID and/or a patID above a certain
- * threshold for certain relations, then the highest rated ones will be chosen,
- * and otherwise some standard ones will be chosen. The chosen setID represents
- * a set of whitelisted URL RegEx patterns, and pattID represents (if it is
- * p0) a blacklist pattern, which can add some final user preferences on top
- * of the chosen set. ..Come to think of it, let us say that two patIDs are
- * looked up: a blacklist pattern and also a whitelist pattern, which.. Hm..
+ * to first of all look up a setID, representing a set of whitelisted URL
+ * patterns (or rather part of the set does when the rating is above a
+ * certain threshold). If the user has rated a setID above a certain
+ * threshold a certain relations, then the highest rated set will be chosen,
+ * and otherwise some standard set will be chosen. The UPA are then not allowed
+ * (and it should not be possible if the API is correctly made) to use links
+ * with URLs that have not first been confirmed to be in this set (and with a
+ * high enough rating). Any further blacklisting and whitelisting preferences
+ * that are specific to the end user must be implemented in the UPA layer.
+ * The set that this program looks up is therefore mainly intended an initial
+ * filter to prevent links to malicious and/or scamming sites. ..Oh wait, but
+ * the UPA can in priciple just change the mentioned ratings, so how to we
+ * prevent it from doing that?.. 
  **/
 
 $err_path = $_SERVER['DOCUMENT_ROOT'] . "/../src/err/";
