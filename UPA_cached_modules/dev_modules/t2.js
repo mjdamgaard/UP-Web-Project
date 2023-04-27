c7b374fd14d0517431ff7c9fd3e6ad34a1ea8568
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-27 12:09:25 +0200

    perhaps almost done with loadContent()..

--- UPA_cached_modules/dev_modules/t2.js
+++ UPA_cached_modules/dev_modules/t2.js
@@ -6,18 +6,42 @@
 
 
 export function loadContent(jqObj) {
-    // get the content key.
+    // get the content key from the jQuery object, its id, and its context data.
     var contentKey = jqObj.attr("content-key");
+    var id = jqObj.attr("id");
+    var contextData = jqObj.attr("context-data");
     // look up the corresponding content loader function in a global variable
     // called upa1_contentLoaderFunctions.
     var clFun = upa1_contentLoaderFunctions[contentKey];
-    // get the context data to pass on to each children.
-    var contextData = jqObj.attr("context-data");
     // call the content loader function on the jQuery object and pass the
     // context data as input as well.
     clFun(jqObj, contextData);
     // after the content is loaded, seach through the children to find any
     // nested content loaders, give them each unique ids (of the form
-    // parentID + uniqueSuffix), .. ..
+    // parentID + uniqueSuffix), and make them each listen for a targeted
+    // event (with their own id prepended) to call this function for them as
+    // well.
+    var idSuffix = 0;
+    jqObj.find('[content-key]').each(function() {
+        // set the id of the child content loader and increase idSuffix.
+        let childID = id + "-" + idSuffix.toString();
+        this.attr("id", childID);
+        idSuffix += 1;
+        // set up a special event listener for the child to load its own
+        // content.
+        this.on(childID + ":Load content", function() {
+            // load the inner content of child.
+            loadContent(this);
+        })
+        // set up a shared event listener for the child to load its own
+        // content (together with all its content loader siblings).
+        this.on(id + ":Load content of children", function() {
+            // remove the targeted event listener.
+            this.off(childID + ":Load content");
+            // load the inner content of child.
+            loadContent(this);
+        })
+        // I'm considering triggering an "I am ready" event at the parent here..
+    });
 
 }
