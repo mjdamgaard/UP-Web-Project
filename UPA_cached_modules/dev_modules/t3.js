b224a2ebc7eb37d8200ee39372f6c44038c36625
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-27 14:23:33 +0200

    Ha, unexpected bug.. (Am I using jQuery wrong..?)

--- UPA_cached_modules/dev_modules/t3.js
+++ UPA_cached_modules/dev_modules/t3.js
@@ -6,7 +6,19 @@ import * as t2Mod from "/UPA_scripts.php?id=t2";
  * initialized to the window by the t2 module.
  **/
 
-// test that upa1_contentLoaderFunctions exists.
+
 upa1_contentLoaderFunctions["categoryTerm"] = function(jqObj, contextData) {
-    jqObj.html("<b>Hello</b>, I am gonna be a category.")
+    jqObj.html("<b>Hello</b>, I'm gonna be a category.")
+        .append('<div content-key="subcategories"></div>')
+        .append('<div content-key="elements" style="float:left"></div>')
+        .on("Children are ready", function() {
+            this.find('> *').trigger("Load content");
+        });
+}
+
+upa1_contentLoaderFunctions["subcategories"] = function(jqObj, contextData) {
+    jqObj.html("<i>Hello</i>, I'm gonna be a subcategory list.");
+}
+upa1_contentLoaderFunctions["elements"] = function(jqObj, contextData) {
+    jqObj.html("And I'm gonna be a list of elements!");
 }
--- UPA_cached_modules/dev_modules/tA.js
+++ UPA_cached_modules/dev_modules/tA.js
@@ -14,10 +14,12 @@ export function upa_main(preferenceUserID, termID, userID) {
     let contextData = JSON.stringify({termID: termID, userID: userID});
     upa1Frame.attr("contextData", contextData);
 
+    upa1Frame.attr("content-key", "categoryTerm");
+
     let termType = termID.substring(0, 1);
     switch (termType) {
         case "c":
-            upa1_contentLoaderFunctions["categoryTerm"](upa1Frame, contextData);
+            upa1_loadContent(upa1Frame);
             break;
         default:
             throw "main(): term type not implemented";
