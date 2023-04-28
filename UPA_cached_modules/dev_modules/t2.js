870fe9d6b42daff933dccd570315cf3d9de060df
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-28 19:08:18 +0200

    Oh, and I probably \emph{don't} need to attach contentSpecs to the window, cause I can instead just.. Hm, I \emph{should} be able to pass it on (via imports) between modules, but I'm not 100 % sure...

--- UPA_cached_modules/dev_modules/t2.js
+++ UPA_cached_modules/dev_modules/t2.js
@@ -62,42 +62,48 @@ export class ContentManager {
     }
 }
 
-function upa1_loadContent(jqObj) {
-    // get the content key from the jQuery object, its id, and its context data.
-    var contentKey = jqObj.attr("content-key");
-    var id = jqObj.attr("id");
-    var contextData = jqObj.attr("context-data");
-    // look up the corresponding content loader function in a global variable
-    // called upa1_contentSpecs.
-    var clFun = upa1_contentSpecs[contentKey];
-    // call the content loader function on the jQuery object and pass the
-    // context data as input as well.
-    clFun(jqObj, contextData);
-    // after the content is loaded, search through the children to find any
-    // nested content loaders, give them each unique ids (of the form
-    // parentID + uniqueSuffix), and make them each listen for an event to
-    // call this function for them as well.
-    var idSuffix = 0;
-    jqObj.find('[content-key]').each(function() {
-        // set the id of the child content loader and increase idSuffix.
-        let childID = id + "." + idSuffix.toString();
-        $(this).attr("id", childID);
-        idSuffix += 1;
-        // set up an event listener for the child to load its own content.
-        $(this).one("load-content", function() {
-            // load the inner content of child.
-            upa1_loadContent($(this));
-            // set boolean attribute to signal that content is loaded.
-            $(this).attr("is-loaded", "true");
-        });
-    });
-    // trigger the loading of the content all the CL children that does not
-    // have the wait attribute.
-    jqObj.find('[content-key]:not([wait])').trigger("load-content");
+
+export class ContentSpecs {
+
 }
 
-// initialize upa1_contentSpecs globally.
-window["upa1_contentSpecs"] = [];
 
-// initialize upa1_loadContent globally.
-window["upa1_loadContent"] = upa1_loadContent;
+// function upa1_loadContent(jqObj) {
+//     // get the content key from the jQuery object, its id, and its context data.
+//     var contentKey = jqObj.attr("content-key");
+//     var id = jqObj.attr("id");
+//     var contextData = jqObj.attr("context-data");
+//     // look up the corresponding content loader function in a global variable
+//     // called upa1_contentSpecs.
+//     var clFun = upa1_contentSpecs[contentKey];
+//     // call the content loader function on the jQuery object and pass the
+//     // context data as input as well.
+//     clFun(jqObj, contextData);
+//     // after the content is loaded, search through the children to find any
+//     // nested content loaders, give them each unique ids (of the form
+//     // parentID + uniqueSuffix), and make them each listen for an event to
+//     // call this function for them as well.
+//     var idSuffix = 0;
+//     jqObj.find('[content-key]').each(function() {
+//         // set the id of the child content loader and increase idSuffix.
+//         let childID = id + "." + idSuffix.toString();
+//         $(this).attr("id", childID);
+//         idSuffix += 1;
+//         // set up an event listener for the child to load its own content.
+//         $(this).one("load-content", function() {
+//             // load the inner content of child.
+//             upa1_loadContent($(this));
+//             // set boolean attribute to signal that content is loaded.
+//             $(this).attr("is-loaded", "true");
+//         });
+//     });
+//     // trigger the loading of the content all the CL children that does not
+//     // have the wait attribute.
+//     jqObj.find('[content-key]:not([wait])').trigger("load-content");
+// }
+//
+// // initialize upa1_contentSpecs globally.
+// window["upa1_contentSpecs"] = [];
+//
+// // initialize upa1_loadContent globally.
+// window["upa1_loadContent"] = upa1_loadContent;
