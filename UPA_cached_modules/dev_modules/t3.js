5cfda9330240aa2f2207d4cc7dded7cf7227c1fb
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-29 11:10:50 +0200

    Oh, but I should still make a ContentSpec class.

--- UPA_cached_modules/dev_modules/t3.js
+++ UPA_cached_modules/dev_modules/t3.js
@@ -56,63 +56,63 @@ upa1_contentSpecs["categoryHeader"][3].push(function(cm, jqObj) {
 
 
 
-//
-// upa1_contentSpecs["categoryTerm"] = function(jqObj, contextData) {
-//     jqObj.html('<div content-key="categoryTermHeader" class="container"></div>')
-//         .append('<div content-key="subcategoryList" class="container"></div>')
-//         .append('<div content-key="elementList" wait class="container"></div>')
-//         .append('<div content-key="categoryTermFooter" class="container"></div>')
-//         // .on("hide-current-list", function() {
-//         //     $(this).children('[content-key$="List"]').hide();
-//         // })
-//         .on("show-subcategory-list", function() {
-//             $(this).children('[content-key$="List"]').hide();
-//             let jqObj = $(this).children('[content-key="subcategoryList"]');
-//             jqObj.show();
-//             if (jqObj.attr("is-loaded") !== "true") {
-//                 jqObj.trigger("load-content");
-//             }
-//         })
-//         .on("show-element-list", function() {
-//             $(this).children('[content-key$="List"]').hide();
-//             let jqObj = $(this).children('[content-key="elementList"]');
-//             jqObj.show();
-//             if (!jqObj.attr("is-loaded")) {
-//                 jqObj.trigger("load-content");
-//             }
-//         });
-// }
-//
-// upa1_contentSpecs["categoryTermHeader"] = function(jqObj, contextData) {
-//     jqObj.html('<ul class="nav nav-tabs"></ul>')
-//         .find('ul')
-//         .append('<li class="active"> <a href="#">Subcategories</a> </li>')
-//         .append('<li> <a href="#">Elements</a> </li>')
-//         .find('li:first-of-type')
-//         .on("click", function() {
-//             $(this).siblings().attr("class", "inactive");
-//             $(this).attr("class", "active")
-//                 .closest('[content-key="categoryTerm"]')
-//                 .trigger("show-subcategory-list");
-//         })
-//         .next()
-//         .on("click", function() {
-//             $(this).siblings().attr("class", "inactive");
-//             $(this).attr("class", "active")
-//                 .closest('[content-key="categoryTerm"]')
-//                 .trigger("show-element-list");
-//         });
-// }
-//
-//
-// upa1_contentSpecs["subcategoryList"] = function(jqObj, contextData) {
-//     jqObj.html('<div hidden content-key="setData"></div>');
-// }
-// upa1_contentSpecs["elementList"] = function(jqObj, contextData) {
-//     jqObj.html('And I am gonna be a list of elements!');
-// }
-//
-//
-// upa1_contentSpecs["categoryTermFooter"] = function(jqObj, contextData) {
-//     jqObj.html("I'm gonna be a footer.");
-// }
+
+upa1_contentSpecs["categoryTerm"] = function(jqObj, contextData) {
+    jqObj.html('<div content-key="categoryTermHeader" class="container"></div>')
+        .append('<div content-key="subcategoryList" class="container"></div>')
+        .append('<div content-key="elementList" wait class="container"></div>')
+        .append('<div content-key="categoryTermFooter" class="container"></div>')
+        // .on("hide-current-list", function() {
+        //     $(this).children('[content-key$="List"]').hide();
+        // })
+        .on("show-subcategory-list", function() {
+            $(this).children('[content-key$="List"]').hide();
+            let jqObj = $(this).children('[content-key="subcategoryList"]');
+            jqObj.show();
+            if (jqObj.attr("is-loaded") !== "true") {
+                jqObj.trigger("load-content");
+            }
+        })
+        .on("show-element-list", function() {
+            $(this).children('[content-key$="List"]').hide();
+            let jqObj = $(this).children('[content-key="elementList"]');
+            jqObj.show();
+            if (!jqObj.attr("is-loaded")) {
+                jqObj.trigger("load-content");
+            }
+        });
+}
+
+upa1_contentSpecs["categoryTermHeader"] = function(jqObj, contextData) {
+    jqObj.html('<ul class="nav nav-tabs"></ul>')
+        .find('ul')
+        .append('<li class="active"> <a href="#">Subcategories</a> </li>')
+        .append('<li> <a href="#">Elements</a> </li>')
+        .find('li:first-of-type')
+        .on("click", function() {
+            $(this).siblings().attr("class", "inactive");
+            $(this).attr("class", "active")
+                .closest('[content-key="categoryTerm"]')
+                .trigger("show-subcategory-list");
+        })
+        .next()
+        .on("click", function() {
+            $(this).siblings().attr("class", "inactive");
+            $(this).attr("class", "active")
+                .closest('[content-key="categoryTerm"]')
+                .trigger("show-element-list");
+        });
+}
+
+
+upa1_contentSpecs["subcategoryList"] = function(jqObj, contextData) {
+    jqObj.html('<div hidden content-key="setData"></div>');
+}
+upa1_contentSpecs["elementList"] = function(jqObj, contextData) {
+    jqObj.html('And I am gonna be a list of elements!');
+}
+
+
+upa1_contentSpecs["categoryTermFooter"] = function(jqObj, contextData) {
+    jqObj.html("I'm gonna be a footer.");
+}
