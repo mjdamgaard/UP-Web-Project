058aae13fe635c64dd7ea3796cc04eb6a4e69dd5
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-29 17:03:23 +0200

    Hm, maybe I should just have a function that converts a contentSpecIndex.. ..Hm, or use this oppertunity to define it as a class with such a method (and a boolean prop telling if it is ready)..

--- UPA_cached_modules/dev_modules/t2.js
+++ UPA_cached_modules/dev_modules/t2.js
@@ -6,7 +6,7 @@
  **/
 
 export class ContentSpec {
-    contructor(tagName, attributes, htmlTemplate, eventSpecs) {
+    constructor(tagName, attributes, htmlTemplate, eventSpecs) {
         this.tagName = tagName;
         this.attributes = attributes ?? {};
         this.htmlTemplate = htmlTemplate;
@@ -18,24 +18,57 @@ export class ContentSpec {
             {method:"on", events:events, handler:handler}
         );
     }
-
     addOneEvent(events, handler) {
         this.eventSpecs.push(
             {method:"one", events:events, handler:handler}
         );
     }
+
+    addLoadInwardEvent(handler) {
+        this.eventSpecs.push(
+            {method:"one", events:"inward", handler:handler}
+        );
+    }
+    addLoadOutwardEvent(handler) {
+        this.eventSpecs.push(
+            {method:"one", events:"outward", handler:handler}
+        );
+    }
+    addLoadReadyEvent(handler) {
+        this.eventSpecs.push(
+            {method:"one", events:"ready", handler:handler}
+        );
+    }
 }
+
+
+
 
 /* Helper function to load the full HTML content from specification, where all
  * "<< <Content Key> >>" occurances are replaces with their corresponding
  * <template> tag variant.
  **/
-function loadAndAppendHTML(jqObj, contentSpec) {
-
+function loadInnerHTML(jqObj, contentSpecIndex, key) {
+    let html = contentSpecIndex[key].htmlTemplate.replaceALL(
+        /<<[^a-z_<>][^<>]*>>/, function(str) {
+            let key = str.slice(2, -2);
+            return '<template content-key="' + key + '"></template>';
+        }
+    );
+    jqObj.html(html)
+}
+function loadOuterHTML(jqObj, contentSpecIndex, key) {
+    let html = contentSpecIndex[key].htmlTemplate.replaceALL(
+        /<<[^a-z_<>][^<>]*>>/, function(str) {
+            let key = str.slice(2, -2);
+            return '<template content-key="' + key + '"></template>';
+        }
+    );
+    jqObj.html(html)
 }
 
 /* The function to load and append content from content specification */
-export function loadAndAppendContent(jqObj, contentSpec) {
+export function loadAndAppendContent(jqObj, contentSpecIndex, key) {
 
 }
 
@@ -45,36 +78,36 @@ export function loadInnerContentTemplates(jqObj) {
 }
 
 
-// function loadAndAppendContent(jqObj, contentSpec) {
-//     // get the content key from the jQuery object, its id, and its context data.
-//     var contentKey = jqObj.attr("content-key");
-//     var id = jqObj.attr("id");
-//     var contextData = jqObj.attr("context-data");
-//     // look up the corresponding content loader function in a global variable
-//     // called upa1_contentSpecs.
-//     var clFun = upa1_contentSpecs[contentKey];
-//     // call the content loader function on the jQuery object and pass the
-//     // context data as input as well.
-//     clFun(jqObj, contextData);
-//     // after the content is loaded, search through the children to find any
-//     // nested content loaders, give them each unique ids (of the form
-//     // parentID + uniqueSuffix), and make them each listen for an event to
-//     // call this function for them as well.
-//     var idSuffix = 0;
-//     jqObj.find('[content-key]').each(function() {
-//         // set the id of the child content loader and increase idSuffix.
-//         let childID = id + "." + idSuffix.toString();
-//         $(this).attr("id", childID);
-//         idSuffix += 1;
-//         // set up an event listener for the child to load its own content.
-//         $(this).one("load-content", function() {
-//             // load the inner content of child.
-//             loadContent($(this));
-//             // set boolean attribute to signal that content is loaded.
-//             $(this).attr("is-loaded", "true");
-//         });
-//     });
-//     // trigger the loading of the content all the CL children that does not
-//     // have the wait attribute.
-//     jqObj.find('[content-key]:not([wait])').trigger("load-content");
-// }
+function loadAndAppendContent(jqObj, contentSpec) {
+    // get the content key from the jQuery object, its id, and its context data.
+    var contentKey = jqObj.attr("content-key");
+    var id = jqObj.attr("id");
+    var contextData = jqObj.attr("context-data");
+    // look up the corresponding content loader function in a global variable
+    // called upa1_contentSpecs.
+    var clFun = upa1_contentSpecs[contentKey];
+    // call the content loader function on the jQuery object and pass the
+    // context data as input as well.
+    clFun(jqObj, contextData);
+    // after the content is loaded, search through the children to find any
+    // nested content loaders, give them each unique ids (of the form
+    // parentID + uniqueSuffix), and make them each listen for an event to
+    // call this function for them as well.
+    var idSuffix = 0;
+    jqObj.find('[content-key]').each(function() {
+        // set the id of the child content loader and increase idSuffix.
+        let childID = id + "." + idSuffix.toString();
+        $(this).attr("id", childID);
+        idSuffix += 1;
+        // set up an event listener for the child to load its own content.
+        $(this).one("load-content", function() {
+            // load the inner content of child.
+            loadContent($(this));
+            // set boolean attribute to signal that content is loaded.
+            $(this).attr("is-loaded", "true");
+        });
+    });
+    // trigger the loading of the content all the CL children that does not
+    // have the wait attribute.
+    jqObj.find('[content-key]:not([wait])').trigger("load-content");
+}
