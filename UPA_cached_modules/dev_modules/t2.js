986a91c9d8f484b41ddd0cdfcf2f3672f052de19
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-29 12:17:22 +0200

    Ah, I think I will actually use the template tag instead. My previous thought was to use a "<<>>" syntax, as in "<<ContentKeyExample>>", but now it will instead be <template content-key="...">, which is not much longer.. ..Oh wait, no, apparently <template> is not an empty tag after all..

--- UPA_cached_modules/dev_modules/t2.js
+++ UPA_cached_modules/dev_modules/t2.js
@@ -6,12 +6,61 @@
  **/
 
 export class ContentSpec {
-    contructor(tagName, attributes, htmlTemplate, eventListeners) {
+    contructor(tagName, attributes, htmlTemplate, eventSpecs) {
+        this.tagName = tagName;
+        this.attributes = attributes ?? {};
+        this.htmlTemplate = htmlTemplate;
+        this.eventSpecs = eventSpecs ?? [];
+    }
 
+    addOnEvent(events, handler) {
+        this.eventSpecs.push(
+            {method:"on", events:events, handler:handler}
+        );
+    }
+
+    addOneEvent(events, handler) {
+        this.eventSpecs.push(
+            {method:"one", events:events, handler:handler}
+        );
     }
 }
 
-function loadContent(jqObj) {
+function loadContent(jqObj, contentSpec) {
+    // get the content key from the jQuery object, its id, and its context data.
+    var contentKey = jqObj.attr("content-key");
+    var id = jqObj.attr("id");
+    var contextData = jqObj.attr("context-data");
+    // look up the corresponding content loader function in a global variable
+    // called upa1_contentSpecs.
+    var clFun = upa1_contentSpecs[contentKey];
+    // call the content loader function on the jQuery object and pass the
+    // context data as input as well.
+    clFun(jqObj, contextData);
+    // after the content is loaded, search through the children to find any
+    // nested content loaders, give them each unique ids (of the form
+    // parentID + uniqueSuffix), and make them each listen for an event to
+    // call this function for them as well.
+    var idSuffix = 0;
+    jqObj.find('[content-key]').each(function() {
+        // set the id of the child content loader and increase idSuffix.
+        let childID = id + "." + idSuffix.toString();
+        $(this).attr("id", childID);
+        idSuffix += 1;
+        // set up an event listener for the child to load its own content.
+        $(this).one("load-content", function() {
+            // load the inner content of child.
+            loadContent($(this));
+            // set boolean attribute to signal that content is loaded.
+            $(this).attr("is-loaded", "true");
+        });
+    });
+    // trigger the loading of the content all the CL children that does not
+    // have the wait attribute.
+    jqObj.find('[content-key]:not([wait])').trigger("load-content");
+}
+
+export function loadContent(jqObj, contentSpec) {
     // get the content key from the jQuery object, its id, and its context data.
     var contentKey = jqObj.attr("content-key");
     var id = jqObj.attr("id");
