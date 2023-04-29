9e23cdd984557cfaaa43759cb233ffbe2ce07731
mjdamgaard <mjdamgaard@outlook.dk>
2023-04-29 11:09:35 +0200

    Oh, and now the last script should define contentSpecs, and that script only; the previous modules should only define and export individual specs (for individual content keys).

--- UPA_cached_modules/dev_modules/t2.js
+++ UPA_cached_modules/dev_modules/t2.js
@@ -5,109 +5,38 @@
  * loader functions are supposed to be inserted (each with a "content key").
  **/
 
-export class ContentManager {
-    contructor(contentKey, cmParent, idArr) {
-        this.contentKey = contentKey;
-        this.cmParent = cmParent;
-        this.idArr = idArr;
-        this.cmChildren = [];
-        this.cmChildrenCounter = 0;
-    }
 
-    get jqObj() {
-        return $(
-            '#' + this.idArr[0] +
-            '> [cm-sibling-id="' +
-                this.idArr.slice(1).join('"] > [cm-sibling-id="') +
-            ']'
-        );
-    }
 
-    loadAndAppendContent(jqObj) {
-        let contentSpecs = upa1_contentSpecs[this.contentKey];
-        let tagName = childContentSpecs[0];
-        let attributes = childContentSpecs[1];
-        let contents = childContentSpecs[2];
-
-        jqObj.append("<" + tagName + "></" + tagName + ">");
-        let newContentContainer = jqObj.children(':last-child')
-            .attr("id", this.idArr.join("."))
-            .attr("cm-sibling-id", this.idArr[this.idArr.length - 1])
-            .attr(attributes);
-
-        let contentsLen = contents.length;
-        for (let i = 0; i < contentsLen; i++) {
-            // if contents[i] starts with "<", assume that it is not a content
-            // key, but is instead HTML to be appended newContentContainer.
-            if (contents[i].substring(0, 1) === "<") {
-                newContentContainer.append(contents);
-            // else if it starts with "html:", assume that the rest is
-            // HTML to be appended to newContentContainer.
-            } else if (contents[i].substring(0, 5) === "html:") {
-                newContentContainer.append(contents.substring(5));
-            // else assume that it is a content key with which to contruct a
-            // new child ContentManager.
-            } else {
-                // initialize a new ContentManager instance as a child of
-                // newContentContainer.
-                let cmChildIDArr = this.idArr.concat([this.cmChildrenCounter]);
-                let cmChild = new ContentManager(contents[i], this, childIDArr);
-                this.cmChildren.push(cmChild);
-                this.cmChildrenCounter++;
-                // call its loadAndAppendContent() method to load its content
-                // and append it to newContentContainer.
-                cmChild.loadAndAppendContent(newContentContainer);
-            }
-        }
-    }
-
-    applyCallbacks() {
-        // TODO..
-    }
+function upa1_loadContent(jqObj) {
+    // get the content key from the jQuery object, its id, and its context data.
+    var contentKey = jqObj.attr("content-key");
+    var id = jqObj.attr("id");
+    var contextData = jqObj.attr("context-data");
+    // look up the corresponding content loader function in a global variable
+    // called upa1_contentSpecs.
+    var clFun = upa1_contentSpecs[contentKey];
+    // call the content loader function on the jQuery object and pass the
+    // context data as input as well.
+    clFun(jqObj, contextData);
+    // after the content is loaded, search through the children to find any
+    // nested content loaders, give them each unique ids (of the form
+    // parentID + uniqueSuffix), and make them each listen for an event to
+    // call this function for them as well.
+    var idSuffix = 0;
+    jqObj.find('[content-key]').each(function() {
+        // set the id of the child content loader and increase idSuffix.
+        let childID = id + "." + idSuffix.toString();
+        $(this).attr("id", childID);
+        idSuffix += 1;
+        // set up an event listener for the child to load its own content.
+        $(this).one("load-content", function() {
+            // load the inner content of child.
+            upa1_loadContent($(this));
+            // set boolean attribute to signal that content is loaded.
+            $(this).attr("is-loaded", "true");
+        });
+    });
+    // trigger the loading of the content all the CL children that does not
+    // have the wait attribute.
+    jqObj.find('[content-key]:not([wait])').trigger("load-content");
 }
-
-
-export class ContentSpec {
-
-}
-
-
-// function upa1_loadContent(jqObj) {
-//     // get the content key from the jQuery object, its id, and its context data.
-//     var contentKey = jqObj.attr("content-key");
-//     var id = jqObj.attr("id");
-//     var contextData = jqObj.attr("context-data");
-//     // look up the corresponding content loader function in a global variable
-//     // called upa1_contentSpecs.
-//     var clFun = upa1_contentSpecs[contentKey];
-//     // call the content loader function on the jQuery object and pass the
-//     // context data as input as well.
-//     clFun(jqObj, contextData);
-//     // after the content is loaded, search through the children to find any
-//     // nested content loaders, give them each unique ids (of the form
-//     // parentID + uniqueSuffix), and make them each listen for an event to
-//     // call this function for them as well.
-//     var idSuffix = 0;
-//     jqObj.find('[content-key]').each(function() {
-//         // set the id of the child content loader and increase idSuffix.
-//         let childID = id + "." + idSuffix.toString();
-//         $(this).attr("id", childID);
-//         idSuffix += 1;
-//         // set up an event listener for the child to load its own content.
-//         $(this).one("load-content", function() {
-//             // load the inner content of child.
-//             upa1_loadContent($(this));
-//             // set boolean attribute to signal that content is loaded.
-//             $(this).attr("is-loaded", "true");
-//         });
-//     });
-//     // trigger the loading of the content all the CL children that does not
-//     // have the wait attribute.
-//     jqObj.find('[content-key]:not([wait])').trigger("load-content");
-// }
-//
-// // initialize upa1_contentSpecs globally.
-// window["upa1_contentSpecs"] = [];
-//
-// // initialize upa1_loadContent globally.
-// window["upa1_loadContent"] = upa1_loadContent;
