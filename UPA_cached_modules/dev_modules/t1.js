2c7378d70831cf40b890caa1523f6b88de728808
mjdamgaard <mjdamgaard@outlook.dk>
2023-05-06 16:55:35 +0200

    ..hm, out-commented that part about looking in its own chache automatically for now, because of the fact that the browser also chaches data.. If I want to reintroduce this part again, I should then remember to also add a method to erase cached data..

--- UPA_cached_modules/dev_modules/t1.js
+++ UPA_cached_modules/dev_modules/t1.js
@@ -44,15 +44,16 @@ export class DBRequestManager {
         if (typeof callback === "undefined") {
             callback = cacheKey;
             cacheKey = null;
-        } else {
-            let cachedValue = this.cache[cacheKey];
-            if (typeof cachedValue !== "undefined") {
-                callback($obj, cachedValue, "success: cached", this.cache);
-            }
-            // TODO: Consider a reimplementation where cacheKey can consist of
-            // several consequtive keys seperated by dots ('.'), where the
-            // lookup then becomes like this.cache[key1][key2]...[keyN].
         }
+        // else {
+        //     let cachedValue = this.cache[cacheKey];
+        //     if (typeof cachedValue !== "undefined") {
+        //         callback($obj, cachedValue, "success: cached", this.cache);
+        //     }
+        //     // TODO: Consider a reimplementation where cacheKey can consist of
+        //     // several consequtive keys seperated by dots ('.'), where the
+        //     // lookup then becomes like this.cache[key1][key2]...[keyN].
+        // }
         let thisDBReqManager = this;
         $.getJSON("query_handler.php", reqData, function(result, textStatus) {
             // unless reqKey == "Set", sanitize the first column of result.
