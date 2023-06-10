0f308ed77f0db303bfde20ecb08afba0ec5f07dd
mjdamgaard <mjdamgaard@outlook.dk>
2023-06-10 10:12:38 +0200

    ..okay, apart from in Creators, it's not actually gonna remove the only type column right now, def_entity_t. But it will mean that data can be sent.. hm..

--- sql/create_open_sdb.sql
+++ sql/create_open_sdb.sql
@@ -160,7 +160,7 @@ CREATE TABLE Terms (
     -- Oh, and more importantly, the specifying entities are used to make
     -- predicates from relation--object pairs, which is of course a central
     -- usage in a semantic system: implementing relations.
-    def_entity_t CHAR(1) NOT NULL,
+    def_entity_t CHAR(1) NOT NULL, 
     def_entity_id BIGINT UNSIGNED,
 
     UNIQUE INDEX (context_id, def_entity_t, def_entity_id, def_str)
