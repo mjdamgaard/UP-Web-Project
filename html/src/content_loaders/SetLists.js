d9a5964ed17e076f72bdd37145cf6f5a732aa59b
mjdamgaard <mjdamgaard@outlook.dk>
2023-06-17 10:12:07 +0200

    Uh, I just got an idea: The listElemDataArr can just be an array of indices into an array containing all the original set elements, sorted after subjID.. well, then I have to.. ah, just add a row with the index of the given setData!..

--- html/src/content_loaders/SetLists.js
+++ html/src/content_loaders/SetLists.js
@@ -14,19 +14,16 @@ import {
 SetList requires data:
     data.elemContentKey,
     data.defaultUserWeightArr,
-    data.predSetDataArr = [predSetData, ...],
-        predSetData = {
+    data.setDataArr = [setData, ...],
+        setData = {
             predCxtID, predStr, objID,
-            userWeightArr?, queryParams?, ratTransFun?,
+            predID?,
+            ratTransFun,
+            userID, weight
+            queryParams,
             setArr?, avgSet?, isReadyArr?
         },
-        userWeightArr = [{userID, weight}, ...],
-        queryParams = {num?, ratingLo?, ratingHi?, offset?, isAscending?},
-    data.defaultQueryNum,
-    data.defaultRatingLo?,
-    data.defaultRatingHi?,
-    data.defaultOffset?,
-    data.defaultIsAscending?,
+        queryParams = {num, ratingLo, ratingHi, offset?, isAscending},
     data.combSet?
     data.initialNum,
     data.incrementNum,
@@ -48,7 +45,7 @@ setListCL.addCallback("data", function(data) {
     data.copyFromAncestor([
         "elemContentKey",
         "defaultUserWeightArr",
-        "predSetDataArr",
+        "setDataArr",
         "defaultQueryNum",
         "defaultRatingLo",  // optional.
         "defaultRatingHi",  // optional.
@@ -76,7 +73,7 @@ setListCL.addCallback(function($ci, data) {
         return false;
     });
     data.setManager = new SetManager(
-        data.defaultQueryNum, data.defaultUserWeightArr, data.predSetDataArr,
+        data.defaultQueryNum, data.defaultUserWeightArr, data.setDataArr,
         data.defaultIsAscending, data.combSet,
     );
     data.setManager.queryAndCombineSets(function(combSet) {
@@ -88,25 +85,25 @@ setListCL.addCallback(function($ci, data) {
 
 export class SetManager {
     constructor(
-        defaultQueryNum, defaultUserWeightArr, predSetDataArr,
+        setDataArr,
         sortAscending, combSet // optional
     ) {
-        this.defaultQueryNum = defaultQueryNum;
-        this.defaultUserWeightArr = defaultUserWeightArr;
-        this.predSetDataArr = predSetDataArr;
+        this.setDataArr = setDataArr;
+        this.setNum = setDataArr.length;
         this.sortAscending = sortAscending ?? 0; // false;
         this.combSet = combSet;
     }
     /*
-    predSetDataArr = [predSetData, ...],
-        predSetData = {
+    Recall:
+    data.setDataArr = [setData, ...],
+        setData = {
             predCxtID, predStr, objID,
             predID?,
-            userWeightArr?, queryParams?, ratTransFun?,
-            setArr?, avgSet?, isReadyArr?
+            userID, weight, ratTransFun, queryParams,
+            set,
+            isReady? ...
         },
-        userWeightArr = [{userID, weight}, ...],
-        queryParams = {num?, ratingLo?, ratingHi?, offset?, isAscending?},
+        queryParams = {num, ratingLo, ratingHi, offset?, isAscending}.
     */
 
     queryAndCombineSets(data, callback) {
@@ -115,59 +112,51 @@ export class SetManager {
             data = null;
         }
         let dbReqManager = sdbInterfaceCL.globalData.dbReqManager;
-        let predNum = this.predSetDataArr.length;
-        for (let i = 0; i < predNum; i++) {
-            let predSetData = this.predSetDataArr[i];
-            let queryParams = (predSetData.queryParams ??= {});
-            // (Using ;; here is just for making Atom syntax highlighting work.)
-            queryParams.num ??= this.defaultQueryNum;;
-            queryParams.ratingLo ??= (this.defaultRatingLo ?? "");;
-            queryParams.ratingHi ??= (this.defaultRatingHi ?? "");;
-            queryParams.offset ??= (this.defaultOffset ?? 0);;
-            queryParams.isAscending ??= (this.sortAscending ? 1 : 0);;
+        let setNum = this.setDataArr.length;
+        for (let i = 0; i < setNum; i++) {
+            let setData = this.setDataArr[i];
+            setData.queryParams.offset ??= 0;
 
-            if (!predSetData.avgSet) {
-                predSetData.userWeightArr ??= this.defaultUserWeightArr;
-                let userNum = predSetData.userWeightArr.length;
-                predSetData.isReadyArr ??= new Array(userNum).fill(false);
+            if (!setData.avgSet) {
+                setData.userWeightArr ??= this.defaultUserWeightArr;
+                let userNum = setData.userWeightArr.length;
+                setData.isReadyArr ??= new Array(userNum).fill(false);
                 // if...
-                if (predSetData.predID) {
-                    this.querySetsAndCombineIfReady(
-                        predSetData, i, data, callback
-                    );
+                if (setData.predID) {
+                    this.querySetsAndCombineIfReady(setData, i, data, callback);
                     continue;
                 }
                 // else...
                 let reqData = {
                     type: "termID",
-                    c: predSetData.predCxtID,
-                    s: encodeURI(predSetData.predStr),
-                    t: predSetData.objID,
+                    c: setData.predCxtID,
+                    s: encodeURI(setData.predStr),
+                    t: setData.objID,
                 };
                 let thisSetManger = this;
                 dbReqManager.query(null, reqData, i, function($ci, result, i) {
-                    predSetData.predID = (result[0] ?? [0])[0];
+                    setData.predID = (result[0] ?? [0])[0];
                     thisSetManger.querySetsAndCombineIfReady(
-                        predSetData, i, data, callback
+                        setData, i, data, callback
                     );
                 });
             }
         }
     }
 
-    querySetsAndCombineIfReady(predSetData, i, data, callback) {
+    querySetsAndCombineIfReady(setData, i, data, callback) {
         let dbReqManager = sdbInterfaceCL.globalData.dbReqManager;
-        predSetData.setArr ??= [];
-        let queryParams = predSetData.queryParams;
-        let userNum = predSetData.userWeightArr.length;
+        setData.setArr ??= [];
+        let queryParams = setData.queryParams;
+        let userNum = setData.userWeightArr.length;
         for (let j = 0; j < userNum; j++) {
-            if (predSetData.isReadyArr[j]) {
+            if (setData.isReadyArr[j]) {
                 continue;
             }
             let reqData = {
                 type: "set",
-                u: predSetData.userWeightArr[j].userID,
-                p: predSetData.predID,
+                u: setData.userWeightArr[j].userID,
+                p: setData.predID,
                 rl: queryParams.ratingLo,
                 rh: queryParams.ratingHi,
                 n: queryParams.num,
@@ -176,30 +165,28 @@ export class SetManager {
             };
             let thisSetManger = this;
             dbReqManager.query(null, reqData, j, function($ci, result, j) {
-                predSetData.setArr[j] = result;
-                predSetData.isReadyArr[j] = true;
+                setData.setArr[j] = result;
+                setData.isReadyArr[j] = true;
                 thisSetManger.computeAvgSetIfReadyAndCombineSetsIfReady(
-                    predSetData, data, callback
+                    setData, data, callback
                 );
             });
         }
-        this.computeAvgSetIfReadyAndCombineSetsIfReady(
-            predSetData, data, callback
-        );
+        this.computeAvgSetIfReadyAndCombineSetsIfReady(setData, data, callback);
     }
 
-    computeAvgSetIfReadyAndCombineSetsIfReady(predSetData, data, callback) {
-        let isReady = predSetData.isReadyArr.reduce(
+    computeAvgSetIfReadyAndCombineSetsIfReady(setData, data, callback) {
+        let isReady = setData.isReadyArr.reduce(
             (acc, val) => acc && val, true
         );
         if (isReady) {
-            this.computeAveragedSet(predSetData);
+            this.computeAveragedSet(setData);
             this.combineSetsIfReady(data, callback);
         }
     }
 
     combineSetsIfReady(data, callback) {
-        let isReady = this.predSetDataArr.reduce(
+        let isReady = this.setDataArr.reduce(
             (acc, val) => acc && val.avgSet, true
         );
         if (isReady) {
@@ -209,14 +196,14 @@ export class SetManager {
     }
 
     // computeAveragedSet() sorts the avgSet after subjID, only if setNum (i.e.
-    // this.predSetDataArr[i].setArr.length) is larger than 1. Otherwise the
+    // this.setDataArr[i].setArr.length) is larger than 1. Otherwise the
     // avgSet will just be result of the single set query.
-    computeAveragedSet(predSetData) {
-        let setArr = predSetData.setArr;
+    computeAveragedSet(setData) {
+        let setArr = setData.setArr;
         // if there is only one set, simply return the set as is.
         let setNum = setArr.length;
         if (setNum === 1) {
-            return predSetData.avgSet = setArr[0];
+            return setData.avgSet = setArr[0];
         }
         // if only one set is non-empty, simply return that set.
         let setLengths = setArr.map(val => val.length);
@@ -224,7 +211,7 @@ export class SetManager {
             (acc, val) => acc + (val === 0 ? 0 : 1), 0
         );
         if (nonEmptyNum === 1) {
-            return predSetData.avgSet = setArr.reduce(
+            return setData.avgSet = setArr.reduce(
                 (acc, val) => acc.length > 0 ? acc : val, []
             );
         }
@@ -233,7 +220,7 @@ export class SetManager {
             setArr[i].sort((row1, row2) => row1[1] - row2[1]);
         }
         // then... TODO: make comments for the rest of the code.
-        let userWeightArr = predSetData.userWeightArr;
+        let userWeightArr = setData.userWeightArr;
         let setLenSum = setLengths.reduce((acc, val) => acc + val, 0);
         let ret = new Array(setLenSum);
         let retLen = 0;
@@ -272,30 +259,30 @@ export class SetManager {
                 }
             }
         }
-        return predSetData.avgSet = ret;
+        return setData.avgSet = ret;
     }
 
     // computeAveragedSet() sorts the combSet according to this.sortAscending.
     computeCombinedSet() {
-        let predSetDataArr = this.predSetDataArr;
-        let predNum = predSetDataArr.length;
-        // if predNum is 1, simply return the avgSet (potentially reversed),
+        let setDataArr = this.setDataArr;
+        let setNum = setDataArr.length;
+        // if setNum is 1, simply return the avgSet (potentially reversed),
         // ignoring the ratTransFun, even if this is defined to something else
         // than x => x.
-        if (predNum <= 1) {
+        if (setNum <= 1) {
             if (
-                predSetDataArr[0].queryParams.isAscending == this.sortAscending
+                setDataArr[0].queryParams.isAscending == this.sortAscending
             ) {
-                return this.combSet = predSetDataArr[0].avgSet;
+                return this.combSet = setDataArr[0].avgSet;
             } else {
-                return this.combSet = predSetDataArr[0].avgSet.toReversed();
+                return this.combSet = setDataArr[0].avgSet.toReversed();
             }
         }
-        // else if predNum is larger than 1, then initialize first the return
+        // else if setNum is larger than 1, then initialize first the return
         // array to the first averaged set, but with an extra third column
         // meant to contain all the averaged ratings before this combination.
-        let ret = predSetDataArr[0].avgSet.map(
-            row => [row[0], row[1], new Array(predNum).fill(row[0])]
+        let ret = setDataArr[0].avgSet.map(
+            row => [row[0], row[1], new Array(setNum).fill(row[0])]
         );
         // for each subsequent avgSet, look for any subjID contained in the
         // first set, and for each one found, apply the setDataArr[i]
@@ -303,9 +290,9 @@ export class SetManager {
         // combRatVal located in the first column of ret. Also store the same
         // averaged ratVal as is in the array in the third column of ret.
         let retLen = ret.length;
-        for (let i = 1; i < predNum; i++) {
-            let ratTransFun = predSetDataArr[i].ratTransFun ?? (x => x);
-            let avgSet = predSetDataArr[i].avgSet;
+        for (let i = 1; i < setNum; i++) {
+            let ratTransFun = setDataArr[i].ratTransFun ?? (x => x);
+            let avgSet = setDataArr[i].avgSet;
             let avgSetLen = avgSet.length;
             let pos = 0;
             for (let j = 0; j < retLen; j++) {
@@ -332,11 +319,11 @@ export class SetManager {
 }
 
 /**
- * getCombinedSet(predSetDataArr) returns a combined set,
+ * getCombinedSet(setDataArr) returns a combined set,
  * combSet = [[combRatVal, subjID, ratValArr], ...]. This is done by first
  * using setAveragedSets() to get averaged sets for each predicate. Then these
  * sets are further combined into one by applying the individual ratTransFuns
- * and then adding up the values. The first predicate in predSetDataArr and the
+ * and then adding up the values. The first predicate in setDataArr and the
  * corresponding averaged set is treated specially in that the combined will
  * contain all the entities of that set and no more. If the other sets contain
  * other entities, these will then not be used for the combined set. And for
@@ -351,7 +338,7 @@ export class SetManager {
 
 // TODO: Correct the following event method if needed and add more event methods
 // to query for more elements of the sets. I imagine a method that simply adds
-// the same number of predSetData to the predSetDataArr, with the same
+// the same number of setData to the setDataArr, with the same
 // predicates (use the predIDs) but with a different offset for all the query-
 // Params. Then one should be able to run queryAndCombineSets() pretty much
 // straight away. But one might also want to add an event method that simply
