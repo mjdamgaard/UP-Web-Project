496e82e6c59ab78eaf2a5c8b66ba32c52854f251
Mads Juul Damgaard <43005772+mjdamgaard@users.noreply.github.com>
2023-07-22 12:29:39 +0200

    Update userdb_config.php

--- src/db_io/userdb_config.php
+++ src/db_io/userdb_config.php
@@ -1,9 +1,11 @@
 <?php
 
-$servername = "localhost"; // Change when the database is running on another
+// (Here 'userDB' refers to the database with the users' credentials.)
+
+$servername = "localhost"; // Change when the database is running on another 
 // server.
-$username = "mads";
-$password = "lemmein";
-$dbname = "mydatabase";
+$username = "Insert userDB usename here!";
+$password = "Insert userDB password here!";
+$dbname = "Insert name of the userDB here!";
 
 ?>
