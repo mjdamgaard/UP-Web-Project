75675fd83377d84946f5f8c6e0b55e05631cdfa4
mjdamgaard <mjdamgaard@outlook.dk>
2023-07-22 17:00:11 +0200

    Testing..

--- src/db_io/sdb_config.php
+++ src/db_io/sdb_config.php
@@ -2,8 +2,8 @@
 
 $servername = "localhost"; // Change when the database is running on another
 // server.
-$username = "mads";
-$password = "lemmein";
-$dbname = "mydatabase";
+$username = "Insert SDB usename here!";
+$password = "Insert SDB password here!";
+$dbname = "Insert name of the SDB here!";
 
 ?>
--- src/db_io/userdb_config.php
+++ src/db_io/userdb_config.php
@@ -1,9 +1,11 @@
 <?php
 
+// (Here 'userDB' refers to the database with the users' credentials.)
+
 $servername = "localhost"; // Change when the database is running on another
 // server.
-$username = "mads";
-$password = "lemmein";
-$dbname = "mydatabase";
+$username = "Insert SDB usename here!";
+$password = "Insert SDB password here!";
+$dbname = "Insert name of the SDB here!";
 
 ?>
