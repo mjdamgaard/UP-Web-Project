170e29862b537197e8f1f7336f2f59c1f8450397
mjdamgaard <mjdamgaard@outlook.dk>
2024-04-22 11:50:03 +0200

    Making the EntityInserter class (not done).

--- src/classes/EntityInserter.js
+++ src/classes/EntityInserter.js
@@ -5,12 +5,79 @@ import {DBRequestManager} from "../classes/DBRequestManager.js";
 
 
 export class EntityInserter {
-  constructor() {
-    this.something = undefined;
+  #idOrCallbackArrStore = {};
+
+  // constructor() {
+
+  // }
+
+
+  insertOrFind(entDefObj) {
+    // If entDefObj is an array, call this method on each element and return.
+    if (Array.isArray(entDefObj)) {
+      entDefObj.forEach(val => (this.insertOrFind(val)));
+      return;
+    }
+
+    // Else check the meta type of the entDefObj, and branch accordingly.
+    switch (entDefObj.metaType) {
+      case 'd':
+        break;
+      case 's':
+        break;
+      case 'f':
+        break;
+      case 'p':
+        break;
+      case 't':
+        break;
+      case 'b':
+        throw "EntityInserter: Binaries are not implemented yet.";
+      case 'u':
+        throw "EntityInserter: Users cannot be inserted.";
+      case 'a':
+        throw "EntityInserter: Aggregation bots cannot be inserted.";
+      default:
+        throw "EntityInserter: Unrecognized meta type.";
+    }
+
   }
 
-  method() {
-    throw "InstListGenerator";
+
+
+  /* The methods below should be treated like private methods. They are
+   * only public because they need to be called from callback functions.
+  **/
+
+
+  storeEntID(key, entID) {
+    let callbackArr = this.#idOrCallbackArrStore[key] ?? [];
+
+    // Verify that the callback array is not already replaced with an ID by
+    // an earlier call to this method.
+    if (typeof callbackArr !== "object") {
+      throw (
+        'EntityInserter: The key "' + key + '" is already used.'
+      );
+    }
+
+    // Call all the callbacks with the entID (and key) as input, then exchange
+    // the callback array for the entID.
+    callbackArr.forEach(callback => {
+      callback(entID, key);
+    });
+    this.#idOrCallbackArrStore[key] = entID;
+    return;
+  }
+  
+  inputEntity(reqData, key) {
+    if (key) {
+      DBRequestManager.input(this, reqData, (obj, result) => {
+        obj.storeEntID(key, result.outID);
+      });
+    } else {
+      DBRequestManager.input(this, reqData, () => {});
+    }
   }
 
 }
