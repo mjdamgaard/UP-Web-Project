53e593a10304e9e71cb2ca3f526dbe142a00a4fc
mjdamgaard <mjdamgaard@outlook.dk>
2024-09-18 11:42:48 +0200

    Oh, I just relalized that calling dispatch inside a reducer will undo the reducer's own state changes..

---
+++ src/classes/LazyDelayedPromise.js
@@ -0,0 +1,59 @@
+
+
+
+
+export class LazyDelayedPromiseOnceRest {
+
+  constructor(delay) {
+    this.delay = delay;
+    this.nonce = 0;
+  }
+  
+  then(callback) {
+    this.nonce++;
+    let newNonce = this.nonce;
+    setTimeout(
+      () => {
+        if (this.nonce === newNonce) {
+          callback();
+        }
+      },
+      this.delay
+    );
+  }
+}
+
+
+// export class LazyDelayedPromise {
+
+//   constructor(delay, isReady) {
+//     this.delay = delay;
+//     this.isReady = isReady ?? false;
+//     this.callbackArr = [];
+//   }
+  
+//   then(callback) {
+//     this.callbackArr.push(callback);
+//     if (this.isReady) {
+//       this.isReady = false;
+//       let lastCallback = this.callbackArr.pop()();
+//       this.callbackArr = [];
+//       lastCallback();
+//       sleep(this.delay).then(() => {
+//         if (!this.isReady) {
+//           this.isReady = true;
+//         }
+//       });
+//     }
+//     else {
+//       sleep(this.delay).then(() => {
+//         if (this.callbackArr.length > 0 && this.isReady) {
+//           this.isReady = false;
+//           let lastCallback = this.callbackArr.pop()();
+//           this.callbackArr = [];
+//           lastCallback();
+//         }
+//       })
+//     }
+//   }
+// }
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,4 +1,4 @@
-import {useState, createContext, useContext, useMemo, useId} from "react";
+import {useState, createContext, useContext, useMemo, useId, useEffect} from "react";
 import {
   useStateAndReducers, useDispatch
 } from "../../contexts_and_hooks/useStateAndReducers.js"
@@ -12,16 +12,22 @@ import {InterfaceHeader} from "../InterfaceHeader.js";
 import {AppColumn} from "../app_columns/AppColumn.js";
 import {ListGeneratorPage} from "../ListGenPages.js";
 
+import {
+  LazyDelayedPromiseOnceRest
+} from "../../classes/LazyDelayedPromise.js";
+
 /* Placeholders */
 // const ListGeneratorPage = () => <template></template>;
 
 
 export const HOME_ENTITY_ID = 12;
 
+
+
 const mainPageReducers = {
   key: "main",
   "OPEN_COLUMN": ([state], [colSpec, callerColKey]) => {
-    const {colKeyArr, specStore, fst, n, nonce} = state;
+    const {colKeyArr, specStore, nonce} = state;
     let callerColInd = colKeyArr.indexOf(callerColKey);
     let newNonce = nonce + 1;
     let newColKeyArr = (callerColInd === -1) ?
@@ -31,29 +37,90 @@ const mainPageReducers = {
       )
     let newSpecStore = {...specStore, [newNonce]: colSpec};
     let newCurrInd = callerColInd + 1;
-    let newFST = (fst < newCurrInd - n + 1) ?
-      newCurrInd - n + 1 :
-      (fst > newCurrInd) ?
-        newCurrInd :
-        fst;
     return {
+      ...state,
       colKeyArr: newColKeyArr,
       specStore: newSpecStore,
-      nonce: newNonce, currInd: newCurrInd, fst: newFST, n: n,
+      nonce: newNonce,
+      currInd: newCurrInd,
     };
   },
+
+  "UPDATE_SCROLL": ([state], scrollLeft, dispatch) => {
+    // Get the scroll velocity.
+    const scrollVelocity = scrollLeft - state.scrollLeft;
+    
+    scrollEndPromise.then(() => {
+      dispatch("self", "UPDATE_SCROLL_END", [scrollLeft, scrollVelocity]);
+    });
+
+    return {...state, scrollLeft: scrollLeft, scrollVelocity: scrollVelocity};
+  },
+
+  "UPDATE_SCROLL_END": ([state], [scrollLeft, scrollVelocity], dispatch) => {
+    // Get the center positions of each app column.
+    const columnContainer = document.querySelector(".column-container");
+    const appColumnWrappers = document.querySelectorAll(
+      "[id^=app-column-wrapper]"
+    );
+    const posArr = [];
+    appColumnWrappers.forEach((element, ind) => {
+      let {left, right} = element.getBoundingClientRect();
+      posArr[ind] = (right - left) / 2;
+    });
+
+    // Get the center position of the column container.
+    const {left, right} = columnContainer.getBoundingClientRect();
+    const center = (right - left) / 2;
+
+    // Get the first column in the scroll direction from the center of the
+    // column container.
+    const {colInd} = posArr.reduce((acc, val, ind) => {
+      let offSetFromCenter = val - center;
+      let absOffSet = Math.abs(offSetFromCenter);
+      if (ind === 0) {
+        return {absOffSet: absOffSet, colInd: 0}
+      }
+      else if (Math.sign(offSetFromCenter) !== Math.sign(scrollVelocity)) {
+        return acc;
+      }
+      else if (absOffSet < acc.absOffSet) {
+        return {absOffSet: absOffSet, colInd: ind};
+      }
+      else {
+        return acc;
+      }
+    });
+    // dispatch("self", "UPDATE_CURRENT_COLUMN", colInd);
+
+    const newCenterColumn = appColumnWrappers.item(colInd);
+    // Test: Let's...
+    newCenterColumn.scrollIntoView();
+
+    return state;
+  },
 }
+
+
+
+const scrollEndPromise = new LazyDelayedPromiseOnceRest(100);
+
+
 
 export const MainPage = (props) => {
   const [{
     colKeyArr,
     specStore,
-    nonce, currInd, fst, n,
+    nonce,
+    currInd,
+    scrollLeft, scrollVelocity
 
   }, dispatch, passData] = useStateAndReducers({
     colKeyArr: [0, 1],
-    specStore: {"0": {entID: HOME_ENTITY_ID}, "1": {entID: HOME_ENTITY_ID}},
-    nonce: 1, currInd: 0, fst: 0, n: 1,
+    specStore: {"0": {entID: HOME_ENTITY_ID}, "1": {entID: 1}},
+    nonce: 1,
+    currInd: 0,
+    scrollLeft: 0, scrollVelocity: 0,
 
   }, props, mainPageReducers);
 
@@ -66,7 +133,9 @@ export const MainPage = (props) => {
   const appColumns = colKeyArr.map((colKey, ind) => {
     let colSpec = specStore[colKey];
     return (
-      <AppColumn key={colKey} colKey={colKey} colSpec={colSpec} />
+      <div id={"app-column-wrapper-" + colKey}>
+        <AppColumn key={colKey} colKey={colKey} colSpec={colSpec} />
+      </div>
     );
   });
 
@@ -74,11 +143,11 @@ export const MainPage = (props) => {
     <div className="main-page">
       <InterfaceHeader
         setAppPage={void(0)}
-        colKeyArr={colKeyArr} specStore={specStore} currInd={currInd} n={n}
+        colKeyArr={colKeyArr} specStore={specStore} currInd={currInd}
       />
       <div className="column-container" onScroll={(event => {
-        // TODO: Implement function that sets the by calling a
-        // dispatch("self", "FOCUS") reducer to do so.
+        let {scrollLeft, scrollRight} = event.target;
+        dispatch("self", "UPDATE_SCROLL", scrollLeft);
         // TODO: Also at some point add click event rather than using the
         // scroll snap property, since it is unresponsive for too long after
         // snapping. (But do this only when it can be tested that it doesn't
