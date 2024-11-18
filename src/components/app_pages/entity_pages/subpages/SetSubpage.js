4e917123acd3c1aef6a47fa6b693696b5937b6a0
mjdamgaard <mjdamgaard@outlook.dk>
2024-11-18 17:49:47 +0100

    Made SetSubpage fetch subjClassID if not provided.

--- src/components/app_pages/entity_pages/subpages/SetSubpage.js
+++ src/components/app_pages/entity_pages/subpages/SetSubpage.js
@@ -7,10 +7,32 @@ import {SubpagesWithTabs} from "./SubpagesWithTabs.js";
 import {EntityList} from "../../../entity_lists/EntityList.js";
 import {ScaleReference} from "../../../entity_refs/EntityReference.js";
 import {ScaleSubpage} from "./ScaleSubpage.js";
+import {DataFetcher} from "../../../../classes/DataFetcher.js";
 
 
 
 export const SetSubpage = ({objID, relID, subjClassID, ElemComp}) => {
+  const [results, setState] = useState({});
+
+  useMemo(() => {
+    if (!subjClassID) {
+      DataFetcher.fetchPublicSmallEntity(
+        relID , (datatype, defStr, len, creatorID, isContained) => {
+          setState(prev => {
+            return {
+              ...prev,
+              datatype: datatype,
+              defStr: defStr,
+              len: len,
+              creatorID: creatorID,
+              isContained: isContained,
+              isFetched: true,
+            };
+          });
+        }
+      );
+    }
+  }, []);
 
   const getPageCompFromID = useCallback(tabID => {
     return (
@@ -21,6 +43,22 @@ export const SetSubpage = ({objID, relID, subjClassID, ElemComp}) => {
       }]
     );
   }, [objID, relID]);
+
+  // Before results is fetched, render this:
+  if (!subjClassID && !results.isFetched) {
+    return (
+      <></>
+    );
+  }
+
+  try {
+    subjClassID = subjClassID ||
+      JSON.parse(results.defStr)["Subject class"].substring(1);
+  } catch (error) {
+    return (
+      <span>Subject class is missing.</span>
+    );
+  }
 
   const initTabsJSON = JSON.stringify([
     [basicEntIDs["qualities/relevant"], "Relevant"],
