afeb24f063210dca7e6fdbbaf132b893b827d2f3
mjdamgaard <mjdamgaard@outlook.dk>
2025-08-18 17:21:07 +0200

    Added an initial section to install the system and upload your first app to the tutorial (still with some placeholders and other todos).

--- dir_uploads/up_directories/tutorials/introduction/main.jsx
+++ dir_uploads/up_directories/tutorials/introduction/main.jsx
@@ -116,8 +116,80 @@ export function render() {
           "and/or third parties, and importantly what part is not.)"
         }</p>
         <p>{
-          "Thus, imagine a web where each individual user gets to choose the " +
-          "designs and underlying algorithms that suits them the best."
+          "Thus, imagine a web where each user gets to " +
+          "choose the design and underlying algorithms of the apps that " +
+          "suits them the best."
+        }</p>
+      </section>
+
+      <section>
+        <h2>{"Getting started"}</h2>
+        <h3>{"Installation"}</h3>
+        <p>{
+          "To carry out the tasks of this tutorial, you need to first of " +
+          "need to install Node.js, if this is not currently installed on " +
+          "your computer. You will need this to execute a Node.js program " +
+          "that lets you upload and and edit the source code that this " +
+          "tutorial asks you to. " +
+          "(Again, you can also choose to follow the tutorial without doing " +
+          "this.) "
+        }</p>
+        <p>{
+          "If Node.js is not yet installed on your computer, you can do so " +
+          "following this instruction: "}<i>{"[TODO: Insert link]"}</i>{"."
+        }</p>
+        <p>{
+          "And then you also need to clone the following GitHub repository: "
+          }<i>{"[TODO: Insert link]"}</i>{
+          ". If you don't know how to do that, here's a quick guide: " +
+          ""}<i>{"[TODO: Insert link]"}</i>{"."
+        }</p>
+        <p>{
+          "And finally, you also need to create a user account if you have " +
+          "not " +
+          "created one already ("}<b>{"no e-mail required"}</b>{" at this " +
+          "point). " +
+          "This should be straightforward to do. Just click on the " +
+          "top-right user icon, and then click \"Sign up.\""
+        }</p>
+
+        <h3>{"Creating your first web app with this framework"}</h3>
+        <p>{
+          "No you are ready to create your first web app with the UP Web " +
+          "framework. To do this, go open a terminal and cd to the GitHub " +
+          "repository that you just downloaded. Then run the following command."
+        }</p>
+        <div><tt>{
+          // TODO: This leads nowhere yet. Make it lead to an app with the
+          // tasks of this tutorial (that the reader will be tasked to edit). 
+          "$ node ./dir_uploads/upload_dir.js ./up_directories/tutorials/intro"
+        }</tt></div>
+        <p>{
+          "This will now prompt you for the username and password of your " +
+          "account. (If you used an auto-generated password, you can find " +
+          "it in your browser's Settings → Password menu.)"
+        }</p>
+        {/* TODO: Insert image here */}
+        <p>{
+          "On success, you now have a simple program where you can type in " +
+          "the command 'u' in order to upload/update your directory, or 'e' " +
+          "to exit the program. Try to upload the directory by typing a " +
+          "'u' in the command line, followed by Enter."
+        }</p>
+        {/* TODO: Insert image here */}
+        <p>{
+          "If that succeeded, great! You have now uploaded your first app " +
+          "with this system. You can go to [TODO: Insert link] to see it, " +
+          "where <id> is the number that was assigned to your new directory. " +
+          "See Fig. 3 for how to find that id. [TODO: Insert figure.]"
+        }</p>
+        {/* TODO: Also implement the home page app such that you can go to
+        specific user-created app via the URL path.*/}
+        <p>{
+          "As you see, your new app currently does nothing other then print " +
+          '"Hello, World!" on the page. [TODO: Insert image of that.] ' +
+          "In the following sections, you will be tasked with editing it to " +
+          "do more than that."
         }</p>
       </section>
     </div>
