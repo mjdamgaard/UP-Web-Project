4bd0fa198142d8437d52b8041bb8866b8d32af58
mjdamgaard <mjdamgaard@outlook.dk>
2025-09-30 10:26:07 +0200

    Working on ScoreInterface.jsx.

--- dir_uploads/up_directories/home_app/scoring/ScoreInterface.jsx
+++ dir_uploads/up_directories/home_app/scoring/ScoreInterface.jsx
@@ -1,5 +1,7 @@
 
+import {toPrecision, parseFloat} from 'number';
 import {fetchMetric, fetchUserScore} from "/1/1/scores.js";
+import {scoreHandler01} from "/1/1/score_handling/ScoreHandler01/em.js";
 
 import * as InputRangeAndValue
 from "../utility_components/InputRangeAndValue.jsx";
@@ -11,7 +13,7 @@ export function render({subjKey, qualKey}) {
   // handler into a component with a prop/context-defined score handler, and
   // then a decorating component which can be used for a request origin
   // whitelist.
-  let scoreHandler = {}; // TODO: Get score handler..
+  let scoreHandler = scoreHandler01;
   let userEntID = this.subscribeToContext("userEntID");
   let {hasBegunFetching, metric, prevScore} = this.state;
   let content;
@@ -22,9 +24,17 @@ export function render({subjKey, qualKey}) {
     fetchMetric(qualKey).then(metric => {
       this.setState(state => ({...state, metric: metric ?? false}));
     });
-    fetchUserScore(qualKey, subjKey, userEntID).then(score => {
-      this.setState(state => ({...state, prevScore: score ?? false}));
-    });
+    if (userEntID) {
+      fetchUserScore(qualKey, subjKey, userEntID).then(score => {
+        this.setState(state => ({...state, prevScore: score ?? false}));
+      });
+    } else {
+      this.setState(state => ({...state, prevScore: false}));
+    }
+    content = <div className="fetching"></div>;
+  }
+
+  else if (metric === undefined || prevScore === undefined) {
     content = <div className="fetching"></div>;
   }
 
@@ -33,11 +43,18 @@ export function render({subjKey, qualKey}) {
 
   // And if the metric is ready, render the score interface.
   else {
+    let hasPrevScore = typeof prevScore === "number";
+    let min = metric["Lower limit"] ?? -10;
+    let max = metric["Upper limit"] ?? 10;
+    let step = parseFloat(toPrecision((max - min) / 100, 3));
     content = <div>
-
-      <InputRangeAndValue key="0"
-        placeholder={prevScore === false ? "N/A" : undefined}
-      />
+      <div>
+        <InputRangeAndValue key="0"
+          value={hasPrevScore ? prevScore : undefined}
+          placeholder={hasPrevScore ? undefined : "N/A"}
+          min={min} max={max} step={step}
+        />
+      </div>
     </div>;
   }
 
