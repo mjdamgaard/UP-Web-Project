ed435480bcf32ddf5042a0e76889beda5e130456
mjdamgaard <mjdamgaard@outlook.dk>
2026-03-22 17:54:38 +0100

    Changing my plans for the app browser.

--- up_directories/semantic_entities/em3.js
+++ up_directories/semantic_entities/em3.js
@@ -1,162 +1,95 @@
 
 
 
-// Categories are a looser type of classes, which first of all aren't used by
-// entity definitions like classes are, and whose members do not have to be
-// instances of the category exactly, but can just anything that's relevant to
-// see when interested in that category. Categories are also only defined by
-// their Name and Description properties, which means that users can very
-// easily add their own ones. And the category names don't have to be plural
-// nouns like is generally the case for classes. So for instance, a sub-category
-// of the 'Apps' category below might be called 'Blogging,' and thus contain
-// not only specific blogs, but also other apps related to blogging.
-export const categories = {
-  "Class": abs("./em1.js;get/classes"),
-  "Name": "Categories",
-  "Superclass": abs("./em1.js;get/entities"),
-  "Common properties": [],
-  "Description": "Categories are defined only by their name and " +
-    "description. Their members do not necessarily have to be of the same" +
-    "class, but can be anything that is relevant to see under the given " +
-    "category.",
-};
 
-
-export const members = {
-  "Class": abs("./em1.js;get/relations"),
-  "Name": "Members",
-  "getQualityName": objKey => "Belongs to ${" + objKey + "}",
-  "getScalarName": (objKey, subjKey) => "${" + subjKey + "} belongs to " +
-    "the category of ${" + objKey + "}",
-  "Object domain": abs("./em3.js;get/categories"),
-  "Subject domain": abs("./em3.js;get/categories"),
-  "Metric": abs("./em1.js;get/gradingMetric"),
-  "Description": abs("./em1_aux.js;get/membersDesc"),
-};
-
-export const subcategories = {
-  "Class": abs("./em1.js;get/relations"),
-  "Name": "Subcategories",
-  "getQualityName": objKey => "Is a subcategory of ${" + objKey + "}",
-  "getScalarName": (objKey, subjKey) => "${" + subjKey + "} is a subcategory " +
-    "of ${" + objKey + "}",
-  "Object domain": abs("./em3.js;get/categories"),
-  "Subject domain": abs("./em3.js;get/categories"),
-  "Metric": abs("./em1.js;get/gradingMetric"),
-  "Description": abs("./em1_aux.js;get/subclassesDesc"),
-};
-
-
-// 'Apps' is a category, where users are meant to be able to easily upload
-// their own subcategories, such as 'Social media,' 'Blogging,' 'Tools,'
-// 'Knowledge sharing,' etc.
-export const appsCat = {
-  "Class": abs("./em3.js;get/categories"),
-  "Name": "Apps",
-  "Description": "All user-programmed apps.",
-};
-
-export const socialMedia = {
-  "Class": abs("./em3.js;get/categories"),
-  "Name": "Social media",
-  "Description": "All apps related to social media.",
-};
-
-export const blogging = {
-  "Class": abs("./em3.js;get/categories"),
-  "Name": "Blogging",
-  "Description": "All apps related to blogging, both specific " +
-    "blogs as well as apps that serves as indexes for blogs.",
-};
-
-
-// Topics is a category that can be used very broadly. It might serve as a good
-// entry into the whole semantic graph, as nearly all things can be grouped
-// under topics. 
-export const topics = {
-  "Class": abs("./em3.js;get/categories"),
-  "Name": "Topics",
-  "Description": "All topics in the world.",
-};
-
-
-
-// The members of this 'Apps' class are not specific JSX components, but rather
-// a set of all JSX components that implements the overall vision for the given
-// app. So the main defining property of an 'App' entity is thus its
-// description, really, along with its name. The entity might also have an
-// "Initial implementation" property in the form of a JSX component, but as the
-// name suggests, this is only meant as a help to showcase an initial prototype
-// of what the vision for the app is.
-// Note that 'Apps' are also meant to be stand-alone apps, and not modules/
-// widgets inside other apps. At some point we might thus introduce a similar
-// class, maybe called 'Widgets' or 'App modules,' which is similar to this
-// class, but instead for all components that are *not* stand-alone apps.
+// Apps are components that are meant to work in a self-contained way, outside
+// of any specific surrounding context, as opposed to components that are
+// supposed to be used within a given app.
 export const apps = {
   "Class": abs("./em1.js;get/classes"),
   "Name": "Apps",
   "Superclass": abs("./em1.js;get/entities"),
   "Common properties": [
-    // Along with the app name, the app entity might also include some
-    // identifier for the version of the app. Note that a 'version' of an app
-    // here does not mean an implementation, but rather an 'App' entity that is
-    // related to the initial 'App' entity of the same name, but has a vision/
-    // description that is altered in some way. Thus, each "version" of an app
-    // might thus also have several implementations.
-    "Version identifier",
 
-    // An optional property pointing to the parent version of the given app.
-    "Parent version",
+    // The "parent class" of an app is a class of apps that implement the same
+    // thing, so to speak. These implementations will typically share the same
+    // app name as the root of their names, possibly followed by version
+    // identifiers. And this means that the name of the given app class should
+    // often just be that same root. For instance, if you have a new idea for
+    // e.g. a SoMe app, call it "FriendBook," then the parent class of all the
+    // 'App' entities that implements this "FriendBook" app ought to be called
+    // simply "FriendBook," whereas the 'App' entities themselves should all be
+    // called "FriendBook" followed by some version identifier(s).
+    // The parent class of an App should always either have this 'Apps' class
+    // as its "Superclass" property, or a class that has this 'Apps' class as a
+    // superclass ancestor.
+    // When a user views an 'App' entity, they should be given the option to
+    // "pin" the app to its "parent class," as well as any superclass ancestors
+    // of that class that the user desires.
+    "Parent class",
 
-    // An initial JSX component that showcases the vision for the app.
-    "Initial implementation",
+    // The JSX component that implements the app.
+    "Component",
   ],
-  "Description": "A class of apps where each member might have more than " +
-    "one implementation. These 'App' entities are thus defined mainly by " +
-    "their name and description, which defines the vision for the given app.",
+  "Description": "The class of all user-programmed apps.",
 };
 
 
-export const versions = {
-  "Class": abs("./em1.js;get/relations"),
-  "Name": "Versions",
-  "getQualityName": objKey => "Is a version of ${" + objKey + "}",
-  "getScalarName": (objKey, subjKey) => "${" + subjKey + "} is a version " +
-    "of ${" + objKey + "}",
-  "Object domain": abs("./em3.js;get/apps"),
-  "Subject domain": abs("./em3.js;get/apps"),
-  "Metric": abs("./em1.js;get/gradingMetric"),
-  "Description": abs("./em1_aux.js;get/membersDesc"),
+// Features *can* be specific components with a specific purpose that appears
+// inside of an app, but they can also be more general things, such as a
+// specific algorithm, or a layout/style of an app, or a new button/option that
+// is added to existing components.    
+export const features = {
+  "Class": abs("./em1.js;get/classes"),
+  "Name": "Features",
+  "Superclass": abs("./em1.js;get/entities"),
+  "Common properties": [
+
+    // Features also have a "parent class" property, which is specifically used
+    // in order for the users to be able to "pin" a feature to its parent class
+    // (and potentially superclasses of that class as well). And this can
+    // potentially be used by the apps that implements the given class of
+    // feature to make the app better tailored specifically to the user, namely
+    // by selecting the specific feature that the user has "pinned" to a given
+    // class with the highest priority. (You should also be able to choose a
+    // "priority" whenever you "pin" an app or a feature.)
+    "Parent class",
+
+    // A route to the implementation of the feature. This might for instance be
+    // a JS module or a JSX module that implements it, but it could also be a
+    // route to a specific function, or something else. That depends on the
+    // kind of feature.
+    "Implementation", 
+
+    // A JSX component that showcases the the feature.
+    "Example component",
+  ],
+  "Description": "A class of all \"features\" that an app might use.",
 };
 
-export const implementations = {
-  "Class": abs("./em1.js;get/relations"),
-  "Name": "Implementations",
-  "getQualityName": objKey => "Is an implementation of ${" + objKey + "}",
-  "getScalarName": (objKey, subjKey) => "${" + subjKey + "} is an " +
-    "implementation of ${" + objKey + "}",
-  "Object domain": abs("./em3.js;get/apps"),
-  "Subject domain": abs("./em1.js;get/components"),
-  "Metric": abs("./em1.js;get/gradingMetric"),
-  "Description": abs("./em1_aux.js;get/membersDesc"),
+
+
+
+
+export const socialMedia = {
+  "Class": abs("./em1.js;get/classes"),
+  "Name": "Social media",
+  "Superclass": abs("./em3.js;get/apps"),
+  "Description": "All apps related to social media.",
 };
 
-export const ideas = {
-  "Class": abs("./em1.js;get/relations"),
-  "Name": "Members",
-  "getQualityName": objKey => "Belongs to ${" + objKey + "}",
-  "getScalarName": (objKey, subjKey) => "${" + subjKey + "} belongs to " +
-    "the category of ${" + objKey + "}",
-  "Object domain": abs("./em3.js;get/apps"),
-  "Subject domain": abs("./em3.js;get/appIdeas"),
-  "Metric": abs("./em1.js;get/gradingMetric"),
-  "Description": abs("./em1_aux.js;get/membersDesc"),
+export const blogging = {
+  "Class": abs("./em1.js;get/classes"),
+  "Name": "Blogging",
+  "Superclass": abs("./em3.js;get/apps"),
+  "Description": "All apps related to blogging, both specific " +
+    "blogs as well as apps that serves as indexes for blogs.",
 };
+
 
 
 // An app idea consists of a text and potentially an example component that
-// showcases the idea (while not fully implementing it yet; otherwise it could
-// just be added as an implementation straightaway).
+// showcases the idea.
 export const appIdeas = {
   "Class": abs("./em1.js;get/classes"),
   "Name": "App ideas",
@@ -169,7 +102,48 @@ export const appIdeas = {
   ],
   "Description": "A class of app ideas, which consist first and foremost of " +
     "a text describing the idea, and potentially also an example component " +
-    "that can showcase the idea (but does not have to be a full " +
-    "implementation of the idea). Note that the example component can also " +
+    "that showcases the idea. Note that the example component can also " +
     "be a whole web page that showcases several different examples within it.",
+};
+
+
+
+export const appIdeasRel = {
+  "Class": abs("./em1.js;get/relations"),
+  "Name": "App ideas",
+  "getQualityName": objKey => "Is a relevant idea for ${" + objKey + "}",
+  "getScalarName": (objKey, subjKey) => "${" + subjKey + "} is a relevant " +
+    "idea for ${" + objKey + "}",
+  "Object domain": abs("./em1.js;get/entities"),
+  "Subject domain": abs("./em3.js;get/appIdeas"),
+  "Metric": abs("./em1.js;get/gradingMetric"),
+  "Description": abs("./em1_aux.js;get/membersDesc"),
+};
+
+
+
+
+// These two following relations might potentially be used in the future, but
+// we'll see.
+export const supportingApps = {
+  "Class": abs("./em1.js;get/relations"),
+  "Name": "Supporting apps",
+  "getQualityName": objKey => "Supports ${" + objKey + "}",
+  "getScalarName": (objKey, subjKey) => "${" + subjKey + "} supports " +
+    "${" + objKey + "}",
+  "Object domain": abs("./em3.js;get/features"),
+  "Subject domain": abs("./em3.js;get/apps"),
+  "Metric": abs("./em1.js;get/gradingMetric"),
+  "Description": abs("./em1_aux.js;get/membersDesc"),
+};
+export const supportedFeatures = {
+  "Class": abs("./em1.js;get/relations"),
+  "Name": "Supported features",
+  "getQualityName": objKey => "Is supported by ${" + objKey + "}",
+  "getScalarName": (objKey, subjKey) => "${" + subjKey + "} is supported by " +
+    "${" + objKey + "}",
+  "Object domain": abs("./em3.js;get/apps"),
+  "Subject domain": abs("./em3.js;get/features"),
+  "Metric": abs("./em1.js;get/gradingMetric"),
+  "Description": abs("./em1_aux.js;get/membersDesc"),
 };
