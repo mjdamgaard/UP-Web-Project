6a28348119c7caafbd13c0ed32a6ec4ea6b71097
mjdamgaard <mjdamgaard@outlook.dk>
2026-07-22 16:19:57 +0200

    Went back to an URL API for the app browser where "ancestor" categories and apps can also be included in the URL.

--- up_directories/app_browser/AppBrowser.jsx
+++ up_directories/app_browser/AppBrowser.jsx
@@ -1,5 +1,6 @@
 
 import {join} from 'string';
+import {indexOf, slice, at, reduce} from 'array';
 import {hasType} from 'type';
 import {fetchEntityID} from "~/../semantic_entities/entities.js";
 import * as AppCategoryPage from "./src/AppCategoryPage.jsx";
@@ -17,25 +18,26 @@ const fetchingPageJSX = <div className="app-browser">
 const appsClassEntPath = abs("~/../semantic_entities/em3.js;get/apps");
 
 
+
 // The URL API for this app browser prototype is that the (tail) URLs of "" and
-// "/apps" both redirect to the default app category page. And the URLs for
-// apps and category pages of the form '/apps/(c|a)/(<entID>|path<entPath>)',
-// where the "a" denote an app page and the "c" denote a category/class. And if
-// the URL ends in '/path<entPath>', we query for the corresponding entity ID
-// (entID) and redirect to the URL that ends in '/<entID>' instead.
+// "/apps" both redirect to the default app category page. And category/class
+// pages have URLs of the form '/apps/cat(/<entID>)+', and app pages have URLs
+// of the form '/apps(/cat(/<entID>)+)?/app/(/<entID>)+'. And if the URL ends
+// in '/path<entPath>', we query for the corresponding entity ID (entID) and
+// redirect to the URL that ends in '/<entID>' instead.
+
 
 export function initialize() {
   return {entIDIsMissing: undefined, curEntPathRef: new MutableArray()};
 }
 
-
 export function render() {
-  let [segment1, segment2, segment3, ...restSegments] = this.getSegments();
+  let [segment1, segment2, ...restSegments] = this.getSegments();
 
   // If the tail URL is empty, or if the second segment is empty, redirect to
   // "~/apps/cat/path" + appsClassEntPath.
   if (!segment1 || segment1 === "apps" && !segment2) {
-    this.replaceURL("~/apps/c/path" + appsClassEntPath);
+    this.replaceURL("~/apps/cat/path" + appsClassEntPath);
     return fetchingPageJSX;
   }
 
@@ -43,22 +45,26 @@ export function render() {
   // what kind of app the URL points to. And we choose 'apps' to be the
   // constant segment for this app browser. (And sub-apps ought to use the same
   // convention unless they start to implement other kinds of apps as well.)
-  if (segment1 !== "apps" || segment2 !== "a" && segment2 !== "c") {
+  if (segment1 !== "apps" || segment2 !== "app" && segment2 !== "cat") {
     return missingPageJSX;
   }
 
-  // If the URL is of the form '/apps/(c|a)/path<entPath>', fetch the
+  // If the URL is of the form '/apps/.../path<entPath>', fetch the
   // corresponding entity ID, and replace the URL.
-  if (segment3 === "path") {
+  let indOfPathSegment = indexOf(restSegments, "path");
+  if (indOfPathSegment !== -1) {
     let {entIDIsMissing, curEntPathRef} = this.state;
-    let entPath = "/" + join(restSegments, "/");
+    let entPath = "/" + join(slice(restSegments, indOfPathSegment), "/");
     if (curEntPathRef[0] === entPath && entIDIsMissing) {
       return missingPageJSX;
     }
     curEntPathRef[0] = entPath;
     fetchEntityID(entPath).then(entID => {
       if (entID) {
-        this.replaceURL("~/apps/" + segment2 + "/" + entID);
+        this.replaceURL(
+          "~/apps/" + segment2 + "/" +
+          join(slice(restSegments, 0, indOfPathSegment), "/") + "/" + entID
+        );
       } else if (curEntPathRef[0] === entPath) {
         this.setState(state => ({...state, entIDIsMissing: true}))
       }
@@ -66,117 +72,56 @@ export function render() {
     return fetchingPageJSX;
   }
 
-  // Else if URL is of the form '/apps/(c|a)/<entID>', validate entID, then
-  // render either an AppPage or an AppCategoryPage depending on the type.
-  let entID = segment3;
-  if (!hasType(entID, "hex")) {
+  // And if it is of the form '/apps(/cat(/<entID>)+)?/app/(/<entID>)+', or
+  // '/apps/cat(/<entID>)+' parse the ancestor category IDs, the ancestor app
+  // IDs, and the final entity ID in the URL.
+  let type, ancCatIDs = [], ancAppIDs = [];
+  let entID = at(restSegments, -1);
+  if (segment2 === "app") {
+    type = "app";
+    ancAppIDs = slice(restSegments, 0, -1);
+  }
+  else {
+    let indOfAppSegment = indexOf(restSegments, "app");
+    if (indOfAppSegment === -1) {
+      type = "cat";
+      ancCatIDs = slice(restSegments, 0, -1);
+    }
+    else {
+      type = "app";
+      ancCatIDs = slice(restSegments, 0, indOfAppSegment);
+      ancAppIDs = slice(restSegments, indOfAppSegment + 1, -1);
+    }
+  }
+
+  // Then validate the extracted entity IDs.
+  let isValid = hasType(entID, "hex") && reduce(
+    ancCatIDs, (acc, val) => acc && hasType(val, "hex"),
+    true
+  ) && reduce(
+    ancAppIDs, (acc, val) => acc && hasType(val, "hex"),
+    true
+  );
+  if (!isValid) {
+    console.error(
+      "Encountered a non-hexadecimal segment in the URL when expecting an " +
+      "entity ID"
+    );
     return missingPageJSX;
   }
-  if (segment2 === "a") {
+
+  if (type === "app") {
     return <div className="app-browser">
-      <AppPage key="a" entID={entID} />
+      <AppPage key="a" entID={entID}
+        ancCatIDs={ancCatIDs} ancAppIDs={ancAppIDs}
+      />
     </div>;
   }
   else {
     return <div className="app-browser">
-      <AppCategoryPage key="c" entID={entID} />
+      <AppCategoryPage key="c" entID={entID}
+        ancCatIDs={ancCatIDs} ancAppIDs={ancAppIDs}
+      />
     </div>;
   }
 }
-
-
-
-// Old:
-
-  // If the second segment is either "app" or "cat", parse the list of
-  // ancestor class IDs, if any, and the ancestor app IDs, if any, as well
-  // as the final entity ID (entID), also recording the type, i.e. "cat" or
-  // "app". That is, unless the URL end in "/path<entPath>", in which case
-  // query for the corresponding entID and redirect to a URL where this part
-  // is replaced with that ID.
-  if (segment2 === "app" || segment2 === "cat") {
-    // First handle any trailing "/path<entPath>" segment group.
-    let indOfPathSegment = indexOf(restSegments, "path");
-    if (indOfPathSegment !== -1) {
-      let {entIDIsMissing} = this.state;
-      if (entIDIsMissing) {
-        return missingPageJSX;
-      }
-      let entPathSegments = sliceArray(restSegments, indOfPathSegment + 1);
-      let initSegments = sliceArray(restSegments, 0, indOfPathSegment);
-      let entPath = "/" + join(entPathSegments, "/");
-      let intiURL = "~/apps/" + segment2 + "/" + join(initSegments, "/");
-      fetchEntityID(entPath).then(entID => {
-        this.trigger("replaceURL", intiURL + "/" + entID);
-      }).catch(err => {
-        this.setState(state => ({...state, entIDIsMissing: true}));
-      });
-      return fetchingPageJSX;
-    }
-
-    // Then divide restSegments into the segments following the "/cat" segment
-    // and the ones following the "/app" segment.
-    let catSegments, appSegments, type;
-    let indOfAppSegment = indexOf(restSegments, "app");
-    if (indOfAppSegment === -1) {
-      if (restSegments[0] !== "cat") {
-        return missingPageJSX;
-      }
-      type = "cat";
-      catSegments = sliceArray(restSegments, 1);
-      appSegments = [];
-    }
-    else if (indOfAppSegment === 0) {
-      type = "app";
-      catSegments = [];
-      appSegments = sliceArray(restSegments, 1);
-    }
-    else {
-      if (restSegments[0] !== "cat") {
-        return missingPageJSX;
-      }
-      type = "app";
-      catSegments = sliceArray(restSegments, 1, indOfAppSegment);
-      appSegments = sliceArray(restSegments, indOfAppSegment + 1);
-    }
-
-    // Then verify that all the catSegments and appSegments are hexadecimal
-    // strings, and extract and subtract the final entID from the relevant
-    // array, such that the remaining catSegments and appSegments arrays now
-    // contains only all the ancestor categories/classes and ancestor apps.
-    let callback = (acc, val) => acc || (hasType(val, "hex") ? false : val);
-    let inValidIDSegment =
-      reduce(catSegments, callback, false) ||
-      reduce(appSegments, callback, false);
-    if (inValidIDSegment) {
-      console.error(
-        'URL contained segment "' + inValidIDSegment +'" that was expected ' +
-        "to be a hexadecimal string"
-      );
-      return missingPageJSX;
-    }
-    let entID, ancestorCatIDs = catSegments, ancestorAppIDs = appSegments;
-    if (!entPath) {
-      if (type === "app") {
-        entID = at(appSegments, -1);
-        ancestorAppIDs = sliceArray(appSegments, 0, -1);
-      }
-      else {
-        entID = at(catSegments, -1);
-        ancestorCatIDs = sliceArray(catSegments, 0, -1);
-      }
-    }
-
-    // And finally, render either an AppPage or an AppCategoryPage depending on
-    // the type, passing the relevant data parsed from the URL.
-    if (type === "app") {
-      return <AppPage key="a" entID={entID}
-        ancestorCatIDs={ancestorCatIDs} ancestorAppIDs={ancestorAppIDs}
-      />;
-    }
-    else {
-      return <AppCategoryPage key="c" entID={entID}
-        ancestorCatIDs={ancestorCatIDs} ancestorAppIDs={ancestorAppIDs}
-      />;
-    }
-  }
